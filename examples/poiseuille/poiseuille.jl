140fbf42fb02facd2de10b960d9abfcb83f7a2af
James Fairbanks <fairbanksj@ufl.edu>
2022-06-26 13:34:14 -0400

    merge pressure simulation into poiseuille flow example

--- examples/poiseuille/poiseuille.jl
+++ examples/poiseuille/poiseuille.jl
@@ -1,6 +1,7 @@
 using CombinatorialSpaces
 using CombinatorialSpaces.ExteriorCalculus
 import Catlab.Theories: otimes, oplus, compose, ⊗, ⊕, ⋅, associate, associate_unit, Ob, Hom, dom, codom
+using CombinatorialSpaces.DiscreteExteriorCalculus: ∧
 using Catlab.Theories
 using Catlab.Present
 using Catlab.Graphics
@@ -146,26 +147,46 @@ function mask_boundary_edges(ds)
   return D
 end
 
-##
-function create_funcs(ds)
 
+opbind(f, T) = Dict(:operator=>f, :type=>T)
+
+function create_funcs(ds, hodge=DiagonalHodge())
   funcs = Dict{Symbol, Dict}()
-  funcs[:⋆₁] = Dict(:operator => ⋆(Val{1}, ds, hodge=DiagonalHodge()),
+  funcs[:⋆₁] = opbind(⋆(Val{1}, ds, hodge=hodge), MatrixFunc())
+  funcs[:⋆₁] = Dict(:operator => ⋆(Val{1}, ds, hodge=hodge),
                     :type => MatrixFunc());
-  funcs[:⋆₀] = Dict(:operator => ⋆(Val{0}, ds, hodge=DiagonalHodge()),
+  funcs[:⋆₀] = Dict(:operator => ⋆(Val{0}, ds, hodge=hodge),
                     :type => MatrixFunc());
-  funcs[:⋆₀⁻¹] = Dict(:operator => inv(⋆(Val{0}, ds, hodge=DiagonalHodge())), #I(nv(ds)), #
+  funcs[:⋆₀⁻¹] = Dict(:operator => inv(⋆(Val{0}, ds, hodge=hodge)), #I(nv(ds)), #
                     :type => MatrixFunc());
-  funcs[:⋆₁⁻¹] = Dict(:operator => inv(⋆(Val{1}, ds, hodge=DiagonalHodge())),
+  funcs[:⋆₁⁻¹] = Dict(:operator => inv(⋆(Val{1}, ds, hodge=hodge)),
                     :type => MatrixFunc());
   funcs[:d₀] = Dict(:operator => d(Val{0}, ds), :type => MatrixFunc());
   funcs[:dual_d₀] = Dict(:operator => dual_derivative(Val{0}, ds), :type => MatrixFunc());
-  funcs[:μ̃] = Dict(:operator => 0.5 *  Diagonal(mask_boundary_edges(ds)), :type => MatrixFunc())
   funcs[:sum₁] = Dict(:operator => (x′, x, y)->(x′ .= x .+ y), :type => InPlaceFunc())
-  funcs[:R] = Dict(:operator => -0.1 * I(ne(ds)), :type => MatrixFunc())
+  funcs[:∧₀₁] = Dict(:operator => (r, c, v) -> r .= -∧(Tuple{0,1}, ds, c, v), :type => InPlaceFunc())
   return funcs
 end
 
+
+##
+function create_funcs(ds, operators, boundaries, hodge=DiagonalHodge())
+  funcs = create_funcs(ds, hodge)
+  merge!(funcs, operators, boundaries)
+  return funcs
+end
+
+function operator_funcs(ds)
+  F = Dict(
+    :μ̃ => Dict(:operator => 0.5 *  Diagonal(mask_boundary_edges(ds)), :type => MatrixFunc()),
+    :R => Dict(:operator => -0.1 * I(ne(ds)), :type => MatrixFunc()),
+    :¬ => Dict(:operator => -I(ne(ds)), :type => MatrixFunc()),
+    :k => Dict(:operator => 1.0 * I(nv(ds)), :type => MatrixFunc()))
+  B = Dict(
+    :∂ρ => Dict(:operator => (ρᵇ, ρ) -> begin ρᵇ .= ρ; ρᵇ[1] = 0; ρᵇ[end] = 0; return ρᵇ end, :type => InPlaceFunc())
+  )
+  create_funcs(ds, F, B)
+end
 form2dim = Dict(:Scalar => x->1,
                 :Form0 => nv,
                 :Form1 => ne,
@@ -181,7 +202,7 @@ add_edge!(s, 1, 2, edge_orientation=true)
 ds = EmbeddedDeltaDualComplex1D{Bool,Float64,Point3D}(s)
 subdivide_duals!(ds, Circumcenter())
 ds
-funcs = create_funcs(ds)
+funcs = operator_funcs(ds)
 func, code = gen_sim(diag2dwd(Poise), funcs, ds; autodiff=false, form2dim=form2dim, params=[:P]);
 prob = ODEProblem(func, [2.], (0.0, 10000.0), [1.,11.])
 sol = solve(prob, Tsit5(); progress=true);
@@ -195,7 +216,7 @@ function linear_pipe(n::Int)
   orient!(s)
   ds = EmbeddedDeltaDualComplex1D{Bool,Float64,Point3D}(s)
   subdivide_duals!(ds, Circumcenter())
-  funcs = create_funcs(ds)
+  funcs = operator_funcs(ds)
   func, _ = gen_sim(diag2dwd(Poise), funcs, ds; autodiff=false, form2dim=form2dim, params=[:P])
   return ds, func, funcs
 end
@@ -224,7 +245,7 @@ function binary_pipe(depth::Int)
   orient!(s)
   ds = EmbeddedDeltaDualComplex1D{Bool,Float64,Point3D}(s)
   subdivide_duals!(ds, Circumcenter())
-  funcs = create_funcs(ds)
+  funcs = operator_funcs(ds)
   func, _ = gen_sim(diag2dwd(Poise), funcs, ds; autodiff=false, form2dim=form2dim, params=[:P])
   return ds, func, funcs
 end
@@ -237,3 +258,65 @@ prob = ODEProblem(func,
 
 sol = solve(prob, Tsit5(); progress=true);
 sol.u
+
+
+@present Poiseuille <: Decapodes1D begin
+  (R, μ̃, ¬)::Hom(Form1(X), Form1(X))
+  k::Hom(Form0(X), Form0(X))
+  # μ̃ = negative viscosity per unit area
+  # R = drag of pipe boundary
+  # k = pressure as a function of density
+  # boundary conditions
+  ∂ρ::Hom(Form0(X), Form0(X))
+end;
+
+Poise = @decapode Poiseuille begin
+  (∇P)::Form1{X}
+  (q, q̇, Δq)::Form1{X}
+  (P, ρ, ρ̇)::Form0{X}
+
+  Δq == d₀{X}(⋆₀⁻¹{X}(dual_d₀{X}(⋆₁{X}(q))))
+  q̇ == sum₁(sum₁(μ̃(Δq), ¬(∇P)),R(q))
+  ∂ₜ{Form1{X}}(q) == q̇
+  ∇P == d₀{X}(P)
+
+  P == k(ρ)
+  ρ̇ == ⋆₀⁻¹{X}(dual_d₀{X}(⋆₁{X}(∧₀₁{X}(ρ,q))))
+  ∂ₜ{Form0{X}}(ρ) == ρ̇
+
+  # boundary conditions
+  ρᵇ::Form0{X}
+  ∂ρ(ρ̇) == ρᵇ
+end;
+
+to_graphviz(Poise, node_labels=true, prog="neato", node_attrs=Dict(:shape=>"oval"))
+
+
+#####
+function linear_pipe(n::Int)
+  s = EmbeddedDeltaSet1D{Bool,Point3D}()
+  add_vertices!(s, n, point=[Point3D(i, 0, 0) for i in 1:n])
+  add_edges!(s, 1:n-1, 2:n, edge_orientation=true)
+  orient!(s)
+  ds = EmbeddedDeltaDualComplex1D{Bool,Float64,Point3D}(s)
+  subdivide_duals!(ds, Circumcenter())
+  funcs = operator_funcs(ds)
+  func, _ = gen_sim(diag2dwd(Poise, in_vars=[:q, :ρ]), funcs, ds; autodiff=false, form2dim=form2dim, params=[:P])
+  return ds, func, funcs
+end
+
+begin
+  ds, func, funcs = linear_pipe(10)
+  prob = ODEProblem(func, [5,3,4,2,5,2,3,4,3, 10,9,8,7,6,5,5,5,5,5], (0.0, 10000.0), [10. *i for i in 1:10])
+  sol = solve(prob, Tsit5(); progress=true);
+  sol.u
+end
+  
+  
+begin
+  ds, func, funcs = linear_pipe(100)
+  prob = ODEProblem(func, vcat(ones(99), 3, 2ones(99)), (0.0, 10000.0), [10. *i for i in 1:10])
+  sol = solve(prob, Tsit5(); progress=true);
+  sol.u
+  sol.u[end][97:102]
+end
