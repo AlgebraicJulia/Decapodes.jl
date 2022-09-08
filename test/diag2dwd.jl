8754efe261fc667ebe239ed254f3a7b7f6911980
James Fairbanks <fairbanksj@ufl.edu>
2022-09-08 17:27:16 -0400

    trying to reimplement diag2dwd

---
+++ test/diag2dwd.jl
@@ -0,0 +1,118 @@
+using Decapodes
+using Catlab.Theories
+import Catlab.Theories: otimes, oplus, compose, ⊗, ⊕, ⋅, associate, associate_unit, Ob, Hom, dom, codom
+using Catlab.Present
+using Catlab.CategoricalAlgebra
+# using Catlab.Graphics
+# using Catlab.Syntax
+using Catlab.WiringDiagrams
+using Catlab.WiringDiagrams.DirectedWiringDiagrams
+using CombinatorialSpaces
+using CombinatorialSpaces.ExteriorCalculus
+# using CombinatorialSpaces.DiscreteExteriorCalculus: ∧
+using LinearAlgebra
+
+using Decapodes.Examples
+using Decapodes.Diagrams
+# using Decapodes.Simulations
+# using Decapodes.Schedules
+
+@present DiffusionSpace2D(FreeExtCalc2D) begin
+  X::Space
+  k::Hom(Form1(X), Form1(X)) # diffusivity of space, usually constant (scalar multiplication)
+  proj₁_⁰⁰₀::Hom(Form0(X) ⊗ Form0(X), Form0(X))
+  proj₂_⁰⁰₀::Hom(Form0(X) ⊗ Form0(X), Form0(X))
+  sum₀::Hom(Form0(X) ⊗ Form0(X), Form0(X))
+  prod₀::Hom(Form0(X) ⊗ Form0(X), Form0(X))
+end
+
+
+Diffusion = @decapode DiffusionSpace2D begin
+    (C, Ċ₁, Ċ₂)::Form0{X}
+    Ċ₁ == ⋆₀⁻¹{X}(dual_d₁{X}(⋆₁{X}(k(d₀{X}(C)))))
+    Ċ₂ == ⋆₀⁻¹{X}(dual_d₁{X}(⋆₁{X}(d₀{X}(C))))
+    ∂ₜ{Form0{X}}(C) == Ċ₁ + Ċ₂
+end
+
+# TODO: Replace the decapode macro with an ADT like this from MLStyle:
+# Body => [Judge | Eqn]
+# Judge => ([var])::Ob
+# Eqn => Term == Term
+# Term => Call(f, Term) | GENERATOR | Tan
+# Tan => ∂ₜ(term)
+
+# TODO: Write an interpreter for this ADT that generates databases over the following schema.
+
+@present SchDecapode(FreeSchema) begin
+    (Var, TVar, Op1, Op2)::Ob
+    (Type, Operator)::AttrType
+    # Name::AttrType
+    src::Hom(Op1, Var)
+    tgt::Hom(Op1, Var)
+    proj1::Hom(Op2, Var)
+    proj2::Hom(Op2, Var)
+    res::Hom(Op2, Var)
+    incl::Hom(TVar, Var)
+
+    op1::Attr(Op1, Operator)
+    op2::Attr(Op2, Operator)
+    type::Attr(Var, Type)
+    # name::Attr(Var, Name)
+end
+
+
+@abstract_acset_type AbstractDecapode 
+
+
+@acset_type Decapode(SchDecapode,
+  index=[:src, :tgt, :res, :incl, :op1, :op2, :type]) <: AbstractDecapode
+
+
+begin
+D = Diffusion
+inputs = [D.ob_map[v] for v in vertices(J.graph)]
+J = dom(D)
+dp = Decapode{Any, Any}()
+
+boxid = Vector{Int}()
+for v in vertices(J.graph)
+    vname = J.graph[v, :vname]
+    Dv = D.ob_map[v]
+    vid = add_part!(dp, :Var, type=(vname,Dv))
+end
+for e in edges(J.graph)
+    De = D.hom_map[e]
+    s = src(J.graph, e)
+    t = tgt(J.graph, e)
+    add_part!(dp, :Op1, src=s, tgt=t, op1=De)
+    # push!(boxid, add_part!(dp, :Op1, src=Ds, tgt=Dt, operator=De))
+end
+
+end
+dp
+
+# begin
+# inputs = [D.ob_map[v] for v in vertices(J.graph)]
+# wd = WiringDiagram(inputs,[])
+# J = dom(Diffusion)
+# D = Diffusion
+
+# boxid = Vector{Int}()
+# for e in edges(J.graph)
+#     De = D.hom_map[e]
+#     Ds = D.ob_map[src(J.graph, e)]
+#     Dt = D.ob_map[src(J.graph, e)]
+#     @show (e, De)
+#     push!(boxid, add_box!(wd, Box(De, [Ds], [Dt])))
+# end
+# for v in vertices(J.graph)
+#     outdeg = length(outneighbors(J.graph, v))
+#     junc = junction_diagram(Ports([D.ob_map[v]]), outdeg, 1)
+#     juncid = add_box!(wd, junc)
+#     for e in outneighbors(J.graph, v)
+#         w = Wire(:copy, Port(D.ob_map[v], OutputPort, 1), Port(D.ob_map[v], InputPort, 1))
+#         add_wire!(wd, w)
+#     end
+# end
+# end
+# wd
