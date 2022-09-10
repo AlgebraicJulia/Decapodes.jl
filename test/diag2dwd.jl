d9dc512152e597c0cfd24778390fcf0b5165effc
James Fairbanks <fairbanksj@ufl.edu>
2022-09-09 22:34:58 -0400

    add decapode compiler reimplementation
    direct to julia code
    some fixes in DecaExpr interpreter.
    
    add NamedDecapode

--- test/diag2dwd.jl
+++ test/diag2dwd.jl
@@ -12,11 +12,14 @@ using CombinatorialSpaces.ExteriorCalculus
 # using CombinatorialSpaces.DiscreteExteriorCalculus: ∧
 using LinearAlgebra
 using MLStyle
+using Base.Iterators
 
 using Decapodes.Examples
 using Decapodes.Diagrams
 # using Decapodes.Simulations
 # using Decapodes.Schedules
+
+DerivOp = Symbol("∂ₜ")
 
 @present DiffusionSpace2D(FreeExtCalc2D) begin
   X::Space
@@ -60,18 +63,21 @@ end
 @present SchDecapode(FreeSchema) begin
     (Var, TVar, Op1, Op2)::Ob
     (Type, Operator)::AttrType
-    # Name::AttrType
     src::Hom(Op1, Var)
     tgt::Hom(Op1, Var)
     proj1::Hom(Op2, Var)
     proj2::Hom(Op2, Var)
     res::Hom(Op2, Var)
     incl::Hom(TVar, Var)
-
+    
     op1::Attr(Op1, Operator)
     op2::Attr(Op2, Operator)
     type::Attr(Var, Type)
-    # name::Attr(Var, Name)
+end
+
+@present SchNamedDecapode <: SchDecapode begin
+    Name::AttrType
+    name::Attr(Var, Name)
 end
 
 @abstract_acset_type AbstractDecapode
@@ -79,8 +85,11 @@ end
 @acset_type Decapode(SchDecapode,
   index=[:src, :tgt, :res, :incl, :op1, :op2, :type]) <: AbstractDecapode
 
+@acset_type NamedDecapode(SchNamedDecapode,
+  index=[:src, :tgt, :res, :incl, :op1, :op2, :type, :name]) <: AbstractDecapode
+
 # to_decapode helper functions
-reduce_lhs!(eq::Eq, d::Decapode, syms::Dict{Symbol, Int}) =
+reduce_lhs!(eq::Eq, d::AbstractDecapode, syms::Dict{Symbol, Int}) =
   let ! = reduce_lhs! # This will be needed once we upgrade to a recursive grammar
     @match eq._1 begin
       Var(x) => syms[x]
@@ -105,8 +114,10 @@ reduce_lhs!(eq::Eq, d::Decapode, syms::Dict{Symbol, Int}) =
         return res_var
       end
       Tan(x) => begin
-        tx = add_part!(d, :TVar, incl=syms[x._1])
-        return syms[x._1]
+        txv = add_part!(d, :Var, type=:infer)
+        tx = add_part!(d, :TVar, incl=txv)
+        tanop = add_part!(d, :Op1, src=syms[x._1], tgt=txv, op1=DerivOp)
+        return txv #syms[x._1]
       end
       Plus(x, y) => begin # TODO: plus is an Op2 so just fold it into App2
         res_var = add_part!(d, :Var, type=:infer)
@@ -118,7 +129,7 @@ reduce_lhs!(eq::Eq, d::Decapode, syms::Dict{Symbol, Int}) =
   end
 # TODO: lots of code duplication between reduce_lhs! and reduce_rhs!
 # The duplicate code should be abstracted into another helper function
-reduce_rhs!(eq::Eq, d::Decapode, syms::Dict{Symbol, Int}, lhs_ref::Int) =
+reduce_rhs!(eq::Eq, d::AbstractDecapode, syms::Dict{Symbol, Int}, lhs_ref::Int) =
   let ! = reduce_rhs! # Again only necessary once we upgrade language
     @match eq._2 begin
       App1(f, x) => add_part!(d, :Op1, src=syms[x._1], tgt=lhs_ref, op1=f)
@@ -137,7 +148,7 @@ same tgt or res map.
 # Just to get up and running, I tagged untyped variables with :infer
 # TODO: write a type checking/inference step for this function to 
 # overwrite the :infer tags
-function to_decapode(e::DecaExpr)::Decapode
+function Decapode(e::DecaExpr)
   d = Decapode{Any, Any}()
   symbol_table = Dict{Symbol, Int}()
   for judgement in e.judgements
@@ -149,6 +160,46 @@ function to_decapode(e::DecaExpr)::Decapode
     reduce_rhs!(eq, d, symbol_table, v)
   end
   return d
+end
+
+function NamedDecapode(e::DecaExpr)
+    d = NamedDecapode{Any, Any, Symbol}()
+    symbol_table = Dict{Symbol, Int}()
+    for judgement in e.judgements
+      var_id = add_part!(d, :Var, name=judgement._1._1, type=judgement._2)
+      symbol_table[judgement._1._1] = var_id
+    end
+    for eq in e.equations
+      v = reduce_lhs!(eq, d, symbol_table)
+      reduce_rhs!(eq, d, symbol_table, v)
+    end
+    fill_names!(d)
+    return d
+end
+"""    fill_names!
+
+add new variable names to all the variables that don't have names.
+"""
+function fill_names!(d::NamedDecapode)
+    bulletcount = 1
+    for i in parts(d, :Var)
+        if !isassigned(d[:,:name],i)
+            d[i,:name] = Symbol("•$bulletcount")
+            bulletcount += 1
+        end
+    end
+    for e in incident(d, :∂ₜ, :op1)
+        s = d[e,:src]
+        t = d[e, :tgt]
+        @show (e, s, t)
+        d[t, :name] = Symbol("$(d[s,:name])̇")
+    end
+    # for s in parts(d, :TVar)
+    #     t = d[s,:incl]
+    #     d[t, :name] = gensym("∂ₜ")
+    #     incident(d, t, :tgt) ∩ 
+    # end
+    return d
 end
 
 # Tests
@@ -165,8 +216,8 @@ eqs = [Eq(Var(:Ċ₁), AppCirc1([:⋆₀⁻¹, :dual_d₁, :⋆₁, :k, :d₀], 
        Eq(Tan(Var(:C)), Plus(Var(:Ċ₁), Var(:Ċ₂)))
 ]
 diffusion_d = DecaExpr(js, eqs)
-diffusion_cset = to_decapode(diffusion_d)
-
+diffusion_cset = Decapode(diffusion_d)
+diffusion_cset_named = NamedDecapode(diffusion_d)
 # A test with expressions on LHS (i.e. temporary variables must be made)
 # TODO: we should be intelligent and realize that the LHS of the first two
 # equations are the same and so can share a new variable
@@ -175,27 +226,141 @@ eqs = [Eq(Plus(Var(:Ċ₁), Var(:Ċ₂)), AppCirc1([:⋆₀⁻¹, :dual_d₁, 
        Eq(Tan(Var(:C)), Plus(Var(:Ċ₁), Var(:Ċ₂)))    
 ]
 test_d = DecaExpr(js, eqs)
-test_cset = to_decapode(test_d)
+test_cset = Decapode(test_d)
+test_cset_named = NamedDecapode(test_d)
 
-#=begin
-D = Diffusion
-inputs = [D.ob_map[v] for v in vertices(J.graph)]
-J = dom(D)
-dp = Decapode{Any, Any}()
+all(isassigned(test_cset_named[:name], i) for i in parts(test_cset_named,:Var))
 
-boxid = Vector{Int}()
-for v in vertices(J.graph)
-    vname = J.graph[v, :vname]
-    Dv = D.ob_map[v]
-    vid = add_part!(dp, :Var, type=(vname,Dv))
-end
-for e in edges(J.graph)
-    De = D.hom_map[e]
-    s = src(J.graph, e)
-    t = tgt(J.graph, e)
-    add_part!(dp, :Op1, src=s, tgt=t, op1=De)
-    # push!(boxid, add_part!(dp, :Op1, src=Ds, tgt=Dt, operator=De))
+sup_js = js = [Judge(Var(:C), :Form0, :X), 
+Judge(Var(:ϕ₁), :Form0, :X),
+Judge(Var(:ϕ₂), :Form0, :X)
+]
+sup_eqs = [Eq(Var(:ϕ₁), AppCirc1([:⋆₀⁻¹, :dual_d₁, :⋆₁, :k, :d₀], Var(:C))),
+       Eq(Var(:ϕ₂), AppCirc1([:⋆₀⁻¹, :dual_d₁, :⋆₁, :d₀], Var(:C))),
+       Eq(Tan(Var(:C)), Plus(Var(:ϕ₁), Var(:ϕ₂)))    
+]
+sup_d = DecaExpr(sup_js, sup_eqs)
+sup_cset = Decapode(sup_d)
+sup_cset_named = NamedDecapode(sup_d)
+
+function compile(d::NamedDecapode, inputs::Vector)
+    input_tuple = :(())
+    append!(input_tuple.args, inputs)
+    input_numbers = incident(d, inputs, :name)
+    visited = falses(nparts(d, :Var))
+    visited[collect(flatten(input_numbers))] .= true
+    consumed1 = falses(nparts(d, :Op1))
+    consumed2 = falses(nparts(d, :Op2))
+    assigns = Expr[]
+    # FIXME: this is a quadratic implementation of topological_sort inlined in here.
+    for iter in 1:(nparts(d, :Op1) + nparts(d,:Op2))
+        for op in parts(d, :Op1)
+            s = d[op, :src]
+            if !consumed1[op] && visited[s]
+                t = d[op, :tgt]
+                sname = d[s, :name]
+                tname = d[t, :name]
+                operator = d[op, :op1]
+                # skip the derivative edges
+                if operator == DerivOp
+                    continue
+                end
+                consumed1[op] = true
+                visited[t] = true
+                # @show t, visited
+                if isa(operator, AbstractArray)
+                    operator = :(compose($operator))
+                end
+                assignment = :($tname = $operator($sname))
+                push!(assigns, assignment)
+            end
+        end
+
+        for op in parts(d, :Op2)
+            arg1 = d[op, :proj1]
+            arg2 = d[op, :proj2]
+            if !consumed2[op] && visited[arg1] && visited[arg2]
+                r = d[op, :res]
+                a1name = d[arg1, :name]
+                a2name = d[arg2, :name]
+                rname  = d[r, :name]
+                operator = d[op, :op2]
+                consumed2[op] = true
+                visited[r] = true
+                if isa(operator, AbstractArray)
+                    operator = :(compose($operator))
+                end
+                assignment = :($rname = $operator($a1name, $a2name))
+                push!(assigns, assignment)
+            end
+        end
+    end
+    ret = :(return)
+    ret.args = d[d[:,:incl], :name]
+    return quote f($(input_tuple)) = begin
+        $(assigns...)
+        $ret
+    end; end
 end
 
+compile(diffusion_cset_named, [:C,])
+compile(test_cset_named, [:C,])
+compile(sup_cset_named, [:C,])
+
+
+begin
+    
 end
-dp=#
+#     varstmt = :(@variables t)
+#     append!(varstmt.args, bnsir[:variable])
+  
+#     paramstmt = :(@parameters)
+#     append!(paramstmt.args, bnsir[:parameter])
+  
+#     diffstmt = :(D = Differential(t))
+  
+#     ϕs = map(parts(bn, :Box)) do b
+#       vars = map(incident(bn, b,:call)) do i
+#         j = bn[i, :arg]
+#         return bn[j, :variable]
+#       end
+#       p = :(*($(bn[b, :parameter])))
+#       append!(p.args, vars)
+#       return :($(Symbol("ϕ$b")) = $p)
+#     end
+  
+#     infs = map(parts(bn, :Qout)) do tv
+#       vars = map(incident(bn, tv, :infusion)) do wa
+#         j = bn[wa, :influx]
+#         return Symbol("ϕ$j")
+#       end
+#       p = :(+())
+#       append!(p.args, vars)
+  
+#       # same for the outfluxes
+#       vars = map(incident(bn, tv, :effusion)) do wn
+#         j = bn[wn, :efflux]
+#         return :(- $(Symbol("ϕ$j")))
+#       end
+#       append!(p.args, vars)
+#       return p
+#     end
+  
+#     zparts = zip(bn[:tanvar], infs)
+  
+#     eqns = [:(D($tanvar) ~ $rhs) for (tanvar, rhs) in zparts]
+#     eq = :([])
+#     append!(eq.args, eqns)
+#     eqnstmt = :(eqs = $eq)
+  
+  
+#     return quote
+#       $varstmt
+#       $paramstmt
+#       $diffstmt
+#       $(ϕs...)
+#       $eqnstmt
+#       return ODESystem(eqs, t, name=:PetriNet)
+#     end
+  
+#   end
