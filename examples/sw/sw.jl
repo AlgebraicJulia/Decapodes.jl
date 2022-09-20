e7cfce671d7cd10782c9341680de04c39d5c3c2b
James P. Fairbanks <fairbanksj@ufl.edu>
2022-09-20 18:16:54 -0400

    SIM: try to make advection on sphere

--- examples/sw/sw.jl
+++ examples/sw/sw.jl
@@ -67,3 +67,63 @@ using GLMakie
 mesh(primal_earth, color=findnode(soln(0), :C), colormap=:plasma)
 mesh(primal_earth, color=findnode(soln(tₑ), :C), colormap=:plasma)
 mesh(primal_earth, color=findnode(soln(tₑ)-soln(0), :C), colormap=:plasma)
+
+AdvDiff = quote
+    C::Form0{X}
+    Ċ::Form0{X}
+    V::Form1{X}
+    ϕ::Form1{X}
+    ϕ₁::Form1{X}
+    ϕ₂::Form1{X}
+
+    # Fick's first law
+    ϕ₁ ==  (d₀∘k)(C)
+    ϕ₂ == ∧₀₁(C,V)
+    ϕ == ϕ₁ + ϕ₂
+    # Diffusion equation
+    Ċ == ∘(⋆₁, dual_d₁,⋆₀⁻¹)(ϕ)
+    ∂ₜ(C) == Ċ
+end
+
+advdiff = parse_decapode(AdvDiff)
+advdiffdp = NamedDecapode(advdiff)
+gensim(expand_operators(advdiffdp), [:C, :V])
+sim = eval(gensim(expand_operators(advdiffdp), [:C, :V]))
+
+fₘ = sim(earth)
+
+# velocity(p) = [-p[2]/p[1], 1.0, 0]/log(abs(p[3])+1)
+velocity(p) = [-p[2]/p[1], 1.0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
+velocity(p) = [0, 0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
+v = flat_op(earth, DualVectorField(velocity.(earth[triangle_center(earth),:dual_point])); dims=[30, 10, Inf])
+c_dist = MvNormal(nploc[[1,2]], 100*[1, 1])
+c = [pdf(c_dist, [p[1], p[2]]) for p in earth[:point]]
+
+u₀ = construct(PhysicsState, [VectorForm(c), VectorForm(1000v)],Float64[], [:C, :V])
+tₑ = 20
+prob = ODEProblem(fₘ,u₀,(0,tₑ))
+soln = solve(prob, Tsit5())
+
+mesh(primal_earth, color=findnode(soln(tₑ), :C), colormap=:plasma)
+
+
+cords(mesh) = begin
+  dim(mesh, i) = [p[i] for p in unique(mesh[:point]) if p[3] > 0]
+  return dim.([mesh], [1,2,3])
+end
+# surface(cords(earth)..., axis=(type=Axis3,))
+# scene = Scene();
+# arr = Makie.arrows!(
+#     cords(earth)..., ones(nv(earth)), ones(nv(earth)), ones(nv(earth));
+#     arrowsize = 10.1, linecolor = (:gray, 0.7), linewidth = 0.02, lengthscale = 0.1
+# )
+
+velocity(p) = [-p[2]/p[1], 1.0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
+ps = earth[:point]
+ns = 100*Vec3f.(map(velocity, ps))
+arrows(
+    ps, ns, fxaa=true, # turn on anti-aliasing
+    linecolor = :gray, arrowcolor = :gray,
+    linewidth = 20.1, arrowsize = 20*Vec3f(3, 3, 4),
+    align = :center, axis=(type=Axis3,)
+)
