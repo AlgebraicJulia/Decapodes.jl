f722be219d82805123ca7fb280ba7c6c966aecd5
James P. Fairbanks <fairbanksj@ufl.edu>
2022-09-21 12:20:02 -0400

    ENH: advection is working?

--- examples/sw/sw.jl
+++ examples/sw/sw.jl
@@ -8,20 +8,29 @@ using MLStyle
 using Distributions
 using LinearAlgebra
 
-function generate(sd, my_symbol)
+function generate(sd, my_symbol; hodge=DiagonalHodge())
+  i0 = (v,x) -> ⋆(1, sd, hodge=hodge)*wedge_product(Tuple{0,1}, sd, v, inv_hodge_star(0,sd, hodge=DiagonalHodge())*x)
   op = @match my_symbol begin
     :k => x->x/20
-    :⋆₀ => x->⋆(0,sd,hodge=DiagonalHodge())*x
-    :⋆₁ => x->⋆(1, sd, hodge=DiagonalHodge())*x
-    :⋆₀⁻¹ => x->inv_hodge_star(0,sd, x; hodge=DiagonalHodge())
-    :⋆₁⁻¹ => x->inv_hodge_star(1,sd,hodge=DiagonalHodge())*x
+    :⋆₀ => x->⋆(0,sd,hodge=hodge)*x
+    :⋆₁ => x->⋆(1, sd, hodge=hodge)*x
+    :⋆₀⁻¹ => x->inv_hodge_star(0,sd, x; hodge=hodge)
+    :⋆₁⁻¹ => x->inv_hodge_star(1,sd,hodge=hodge)*x
     :d₀ => x->d(0,sd)*x
     :dual_d₀ => x->dual_derivative(0,sd)*x
     :dual_d₁ => x->dual_derivative(1,sd)*x
     :∧₀₁ => (x,y)-> wedge_product(Tuple{0,1}, sd, x, y)
     :plus => (+)
+    :(-) => x-> -x
+    # :L₀ => (v,x)->dual_derivative(1, sd)*(i0(v, x))
+    :L₀ => (v,x)->begin
+      dual_derivative(1, sd)*⋆(1, sd)*wedge_product(Tuple{1,0}, sd, v, x)
+      # ⋆(1, sd)*wedge_product(Tuple{1,0}, sd, v, x)
+    end
+    :i₀ => i0 
+    :debug => (args...)->begin println(args[1], length.(args[2:end])) end
   end
-  # return (args...) -> begin println("applying $my_symbol"); println("arg length $(length(args[1]))"); op(args...);end
+  # return (args...) -> begin println("applying $my_symbol"); println("arg length $(length.(args))"); op(args...);end
   return (args...) ->  op(args...)
 end
 
@@ -68,44 +77,75 @@ mesh(primal_earth, color=findnode(soln(0), :C), colormap=:plasma)
 mesh(primal_earth, color=findnode(soln(tₑ), :C), colormap=:plasma)
 mesh(primal_earth, color=findnode(soln(tₑ)-soln(0), :C), colormap=:plasma)
 
+begin
 AdvDiff = quote
     C::Form0{X}
     Ċ::Form0{X}
+    Ċ₁::Form0{X}
+    Ċ₂::Form0{X}
     V::Form1{X}
-    ϕ::Form1{X}
+    # ϕ::Form1{X}
     ϕ₁::Form1{X}
     ϕ₂::Form1{X}
-
+    starC::DualForm2{X}
+    lvc::Form1{X}
+    # _::Null{X}
     # Fick's first law
-    ϕ₁ ==  (d₀∘k)(C)
-    ϕ₂ == ∧₀₁(C,V)
-    ϕ == ϕ₁ + ϕ₂
-    # Diffusion equation
-    Ċ == ∘(⋆₁, dual_d₁,⋆₀⁻¹)(ϕ)
+    ϕ₁ ==  ∘(d₀,k,⋆₁)(C)
+    Ċ₁ == ∘(dual_d₁,⋆₀⁻¹)(ϕ₁)
+
+    # Advective Flux
+    ϕ₂ == -(L₀(V, C))
+    Ċ₂ == ⋆₀⁻¹(ϕ₂)
+    # Superposition Principle
+    # ϕ == plus(ϕ₁ , ϕ₂)
+    # Conservation of Mass
+    Ċ == Ċ₁ + Ċ₂
     ∂ₜ(C) == Ċ
 end
 
 advdiff = parse_decapode(AdvDiff)
 advdiffdp = NamedDecapode(advdiff)
 gensim(expand_operators(advdiffdp), [:C, :V])
+end 
+begin
 sim = eval(gensim(expand_operators(advdiffdp), [:C, :V]))
 
 fₘ = sim(earth)
 
 # velocity(p) = [-p[2]/p[1], 1.0, 0]/log(abs(p[3])+1)
 velocity(p) = [-p[2]/p[1], 1.0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
-velocity(p) = [0, 0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
+# velocity(p) = [0, 0, sign(p[1]*abs(p[3]))]#/log(abs(p[3])+1)
 v = flat_op(earth, DualVectorField(velocity.(earth[triangle_center(earth),:dual_point])); dims=[30, 10, Inf])
-c_dist = MvNormal(nploc[[1,2]], 100*[1, 1])
-c = [pdf(c_dist, [p[1], p[2]]) for p in earth[:point]]
+c_dist = MvNormal([radius/√(2), radius/√(2)], 20*[1, 1])
+c = 100*[pdf(c_dist, [p[1], p[2]]) for p in earth[:point]]
 
 u₀ = construct(PhysicsState, [VectorForm(c), VectorForm(1000v)],Float64[], [:C, :V])
-tₑ = 20
+mesh(primal_earth, color=findnode(u₀, :C), colormap=:plasma)
+tₑ = 4.5
 prob = ODEProblem(fₘ,u₀,(0,tₑ))
 soln = solve(prob, Tsit5())
+end
+
+Figure()
+scatter(0:tₑ/150:tₑ, [norm(findnode(soln(t), :C)) for t in 0:tₑ/150:tₑ ])
 
 mesh(primal_earth, color=findnode(soln(tₑ), :C), colormap=:plasma)
+begin
+# Plot the result
+times = range(0.0, tₑ, length=150)
+colors = [findnode(soln(t), :C) for t in times]
 
+# Initial frame
+fig, ax, ob = mesh(primal_earth, color=colors[1], colorrange = extrema(vcat(colors...)))
+Colorbar(fig[1,2], ob)
+framerate = 5
+
+# Animation
+record(fig, "diff_adv.gif", range(0.0, tₑ; length=150); framerate = 30) do t
+    ob.color = findnode(soln(t), :C)
+end
+end
 
 cords(mesh) = begin
   dim(mesh, i) = [p[i] for p in unique(mesh[:point]) if p[3] > 0]
