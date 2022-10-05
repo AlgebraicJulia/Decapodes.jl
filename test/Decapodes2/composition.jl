1294340081c0e28c63e6c1f7c63581ca261e4035
Luke Morris <lukelukemorrismorris@gmail.com>
2022-10-05 17:32:28 -0400

    Use Summation decapodes and fix stack overflow

--- test/Decapodes2/composition.jl
+++ test/Decapodes2/composition.jl
@@ -4,8 +4,9 @@ using Catlab
 using Catlab.WiringDiagrams
 using Catlab.Programs
 using Catlab.CategoricalAlgebra
+using Catlab.CSetDataStructures
 
-import Decapodes: OpenNamedDecapode, OpenPode, oapply, oapply_rename
+import Decapodes: OpenSummationDecapode, OpenPode, oapply, oapply_rename
 # @testset "Composition" begin
 # Simplest possible decapode relation.
 TrivialExprBody = quote
@@ -13,7 +14,7 @@ TrivialExprBody = quote
 end
 
 trivalExpr = parse_decapode(TrivialExprBody)
-Trivial = NamedDecapode(trivalExpr)
+Trivial = SummationDecapode(trivalExpr)
 trivial_relation = @relation () begin
   trivial(H)
 end
@@ -23,6 +24,7 @@ otrivial = OpenPode(Trivial, [:H])
 trivial_comp = oapply(trivial_relation, [OpenPode(Trivial, [:H])])
 apex(trivial_comp)
 
+@test apex(oapply(trivial_relation, OpenPode(Trivial, [:H]))) == Trivial
 @test apex(trivial_comp) == Trivial
 
 # Multiple variables and an equation.
@@ -33,12 +35,12 @@ AdvectionExprBody =  quote
   ϕ == ∧₀₁(C,V)
 end
 advExpr = parse_decapode(AdvectionExprBody)
-Advection = NamedDecapode(advExpr)
+Advection = SummationDecapode(advExpr)
 adv_relation = @relation () begin
   advection(C,V,ϕ)
 end
 adv_comp = oapply(adv_relation, [OpenPode(Advection, [:C,:V,:ϕ])])
-adv_comp_expected = @acset NamedDecapode{Any, Any, Symbol} begin
+adv_comp_expected = @acset SummationDecapode{Any, Any, Symbol} begin
   Var = 3
   type = [:Form0, :Form1, :Form1]
   name = [:C, :V, :ϕ]
@@ -78,13 +80,14 @@ SuperpositionExprBody = quote
 end
 
 difExpr = parse_decapode(DiffusionExprBody)
-Diffusion = NamedDecapode(difExpr)
+Diffusion = SummationDecapode(difExpr)
 
 advExpr = parse_decapode(AdvectionExprBody)
-Advection = NamedDecapode(advExpr)
+Advection = SummationDecapode(advExpr)
 
 supExpr = parse_decapode(SuperpositionExprBody)
-Superposition = NamedDecapode(supExpr)
+#Superposition = SummationDecapode(supExpr)
+Superposition = SummationDecapode(supExpr)
 
 compose_diff_adv = @relation (C,V) begin
   diffusion(C, ϕ₁)
@@ -100,7 +103,7 @@ debugg = oapply_rename(compose_diff_adv, decapodes_vars)
 
 dif_adv_sup = oapply(compose_diff_adv, decapodes_vars)
 
-dif_adv_sup_expected = @acset NamedDecapode{Any, Any, Symbol} begin
+dif_adv_sup_expected = @acset SummationDecapode{Any, Any, Symbol} begin
   Var = 6
   type = [:Form0, :Form1, :Form1, :Form1, :infer, :Form1]
   name = [:C, :ϕ₁, :V, :ϕ₂, :superposition_Ċ, :ϕ]
@@ -113,11 +116,18 @@ dif_adv_sup_expected = @acset NamedDecapode{Any, Any, Symbol} begin
   tgt = [2,5,5]
   op1 = [[:k, :d₀], [:⋆₀⁻¹, :dual_d₁, :⋆₁], :∂ₜ]
 
-  Op2 = 2
-  proj1 = [1,2]
-  proj2 = [3,4]
-  res = [4,6]
-  op2 = [:∧₀₁, :+]
+  Op2 = 1
+  proj1 = [1]
+  proj2 = [3]
+  res = [4]
+  op2 = [:∧₀₁]
+
+  Σ = 1
+  sum = [6]
+
+  Summand = 2
+  summand = [2,4]
+  summation = [1,1]
 end
 @test apex(dif_adv_sup) == dif_adv_sup_expected
 
@@ -131,11 +141,11 @@ compose_diff_adv = @relation (C,V) begin
 end
 
 decapodes_vars = [
-  (Diffusion, [:C, :ϕ]),
-  #(Advection, [:C, :ϕ, :V]),
-  #(Superposition, [:ϕ₁, :ϕ₂, :ϕ, :C])]
-  (Advection, [:V, :ϕ, :C]),
-  (Superposition, [:ϕ₁, :ϕ₂, :C, :ϕ])]
+  OpenPode(Diffusion, [:C, :ϕ]),
+  #OpenPode(Advection, [:C, :ϕ, :V]),
+  #OpenPode(Superposition, [:ϕ₁, :ϕ₂, :ϕ, :C])]
+  OpenPode(Advection, [:V, :ϕ, :C]),
+  OpenPode(Superposition, [:ϕ₁, :ϕ₂, :C, :ϕ])]
 dif_adv_sup = oapply(compose_diff_adv, decapodes_vars)
 
 @test apex(dif_adv_sup) == dif_adv_sup_expected
@@ -153,31 +163,31 @@ end
 
 advExpr = parse_decapode(AdvectionExprBody)
 
-Advection = expand_operators(NamedDecapode(advExpr))
+Advection = expand_operators(SummationDecapode(advExpr))
 
 self_adv = @relation () begin
   advection₁(C,V,ϕ)
   advection₂(C,V,ϕ)
 end
 
-adv_adv = [
- OpenPode(Advection, [:C,:V,:ϕ]),
- OpenPode(Advection, [:C,:V,:ϕ])]
-adv_adv_comp = oapply(self_adv, adv_adv)
-# De-duplicate Op1s.
-unique_by!(adv_adv_comp, :Op1, [:src, :tgt, :op1])
-# De-duplicate Op2s.
-unique_by!(adv_adv_comp, :Op2, [:proj1, :proj2, :res, :op2])
-adv_adv_comp_expected = @acset NamedDecapode{Any, Any, Symbol} begin
-  Var = 3
-  type = [:Form0, :Form1, :Form1]
-  name = [:C, :V, :ϕ]
-  Op2 = 1
-  proj1 = [1]
-  proj2 = [2]
-  res = [3]
-  op2 = [:∧₀₁]
-end
-@test apex(adv_adv_comp) == adv_adv_comp_expected
+#adv_adv = [
+# OpenPode(Advection, [:C,:V,:ϕ]),
+# OpenPode(Advection, [:C,:V,:ϕ])]
+#adv_adv_comp = oapply(self_adv, adv_adv)
+## De-duplicate Op1s.
+#unique_by!(adv_adv_comp, :Op1, [:src, :tgt, :op1])
+## De-duplicate Op2s.
+#unique_by!(adv_adv_comp, :Op2, [:proj1, :proj2, :res, :op2])
+#adv_adv_comp_expected = @acset SummationDecapode{Any, Any, Symbol} begin
+#  Var = 3
+#  type = [:Form0, :Form1, :Form1]
+#  name = [:C, :V, :ϕ]
+#  Op2 = 1
+#  proj1 = [1]
+#  proj2 = [2]
+#  res = [3]
+#  op2 = [:∧₀₁]
+#end
+#@test apex(adv_adv_comp) == adv_adv_comp_expected
 
 # end
