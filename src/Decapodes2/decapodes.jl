a40c65b832a5911dd3226c0eaf877add806482c1
James <fairbanksj@ufl.edu>
2022-10-07 17:48:51 -0400

    Decapodes2 composition with oapply
    * Add initial comp example with openacsettypes
    * Fix advdiff composition test and add op duplication case
    * Deduplicate Op1s and Op2s
    * Add type checking and overwrote copies with junction names
    * Require explicit ordering of vars
    * Do namespacing by prepending box names
    * Compose with oapply
    * Split deduplication into a function and edit tests
    * Imported feet from StructuredMulticospan
    * Use summation decs and fix stack overflow
    * Fix apex pointer issue
    * Pass types through OpenPode constructor
    * Add expand_operators composition test
    * Export composition functions
    * Fix dispatch on oapply for decapodes
    
    Co-authored-by: Luke Morris <lukelukemorrismorris@gmail.com>
    Co-authored-by: GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
    Co-authored-by: Luke Morris <70283489+lukem12345@users.noreply.github.com>
    Co-authored-by: George <grauta@ufl.edu>

--- src/Decapodes2/decapodes.jl
+++ src/Decapodes2/decapodes.jl
@@ -2,6 +2,7 @@ using Catlab
 using Catlab.Theories
 import Catlab.Theories: otimes, oplus, compose, ⊗, ⊕, ⋅, associate, associate_unit, Ob, Hom, dom, codom
 using Catlab.Present
+using Catlab.Programs
 using Catlab.CategoricalAlgebra
 using Catlab.WiringDiagrams
 using Catlab.WiringDiagrams.DirectedWiringDiagrams
@@ -17,7 +18,8 @@ export normalize_unicode, DerivOp, append_dot,
   SchDecapode, SchNamedDecapode, AbstractDecapode, AbstractNamedDecapode, Decapode, NamedDecapode, SummationDecapode, fill_names!, expand_operators,
   Term, Var, Judge, Eq, AppCirc1, AppCirc2, App1, App2, Plus, Tan, term, parse_decapode,
   VectorForm, PhysicsState, findname, findnode,
-  compile, compile_env, gensim, closest_point, flat_op
+  compile, compile_env, gensim, closest_point, flat_op,
+  OpenPode, OpenSummationDecapodeOb, OpenSummationDecapode, unique_by, unique_by!, oapply
 
 normalize_unicode(s::String) = Unicode.normalize(s, compose=true, stable=true, chartransform=Unicode.julia_chartransform)
 normalize_unicode(s::Symbol)  = Symbol(normalize_unicode(String(s)))
@@ -26,5 +28,6 @@ append_dot(s::Symbol) = Symbol(string(s)*'\U0307')
 
 include("decapodeacset.jl")
 include("language.jl")
+include("composition.jl")
 include("visualization.jl")
 include("simulation.jl")
