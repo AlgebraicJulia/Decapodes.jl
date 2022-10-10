a78662d21b03dab3d807d07a76b61660196166da
Luke Morris <lukelukemorrismorris@gmail.com>
2022-10-10 12:09:09 -0400

    Move Dec2 tests, move coordinates.jl, and fix hypot bug

--- test/Decapodes2/runtests.jl
+++ test/Decapodes2/runtests.jl
@@ -4,3 +4,23 @@ using Test
   include(joinpath(@__DIR__, "composition.jl"))
 end
 
+@testset "Coordinates" begin
+  include(joinpath(@__DIR__, "coordinates.jl"))
+end
+
+@testset "Diag2DWD" begin
+  include(joinpath(@__DIR__, "diag2dwd.jl"))
+end
+
+@testset "Meshes" begin
+  include(joinpath(@__DIR__, "meshes.jl"))
+end
+
+@testset "MultiScaleArrays" begin
+  include(joinpath(@__DIR__, "multiscalearrays.jl"))
+end
+
+@testset "Summation" begin
+  include(joinpath(@__DIR__, "summation.jl"))
+end
+
