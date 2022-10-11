d1d8ed9ddd8b27e7ef7222310ce7a271ff64a2e6
Luke Morris <lukelukemorrismorris@gmail.com>
2022-10-11 11:38:26 -0400

    Use loadmesh on the new icosphers

--- src/Decapodes2/meshes.jl
+++ src/Decapodes2/meshes.jl
@@ -6,15 +6,22 @@ using FileIO
 
 abstract type AbstractMeshKey end
 
-struct UnitIcosphere   <: AbstractMeshKey end
-struct ThermoIcosphere <: AbstractMeshKey end
-struct UnitUVSphere    <: AbstractMeshKey end
-struct ThermoUVSphere  <: AbstractMeshKey end
+#struct UnitIcosphere   <: AbstractMeshKey end
+#struct ThermoIcosphere <: AbstractMeshKey end
+#struct UnitUVSphere    <: AbstractMeshKey end
+#struct ThermoUVSphere  <: AbstractMeshKey end
+
+struct UnitIcosphere1 <: AbstractMeshKey end
+struct UnitIcosphere2 <: AbstractMeshKey end
+struct UnitIcosphere3 <: AbstractMeshKey end
+struct UnitIcosphere4 <: AbstractMeshKey end
+struct UnitIcosphere5 <: AbstractMeshKey end
+
 
 #loadmesh(meshkey::AbstractMeshKey)::EmbeddedDeltaSet2D
 
 loadmesh_helper(obj_file_name) = EmbeddedDeltaSet2D(
-  joinpath(artifact"all_meshes", obj_file_name))
+  joinpath(artifact"all_meshes2", obj_file_name))
 
 #function loadmesh(meshkey::UnitIcosphere)::EmbeddedDeltaSet2D
 #  all_meshes = artifact"all_meshes"
@@ -22,19 +29,24 @@ loadmesh_helper(obj_file_name) = EmbeddedDeltaSet2D(
 #  mesh_obj = FileIO.load(File{format"OBJ"}(joinpath(all_meshes, this_mesh)))
 #  mesh = EmbeddedDeltaSet2D(mesh_obj)
 #end
-function loadmesh(meshkey::UnitIcosphere)::EmbeddedDeltaSet2D
-  loadmesh_helper("unit_icosphere.obj")
+
+function loadmesh(meshkey::UnitIcosphere1)::EmbeddedDeltaSet2D
+  loadmesh_helper("UnitIcosphere1.obj")
 end
 
-function loadmesh(meshkey::ThermoIcosphere)::EmbeddedDeltaSet2D
-  loadmesh_helper("thermo_icosphere.obj")
+function loadmesh(meshkey::UnitIcosphere2)::EmbeddedDeltaSet2D
+  loadmesh_helper("UnitIcosphere2.obj")
 end
 
-function loadmesh(meshkey::UnitUVSphere)::EmbeddedDeltaSet2D
-  loadmesh_helper("unit_tie_gcm.obj")
+function loadmesh(meshkey::UnitIcosphere3)::EmbeddedDeltaSet2D
+  loadmesh_helper("UnitIcosphere3.obj")
 end
 
-function loadmesh(meshkey::ThermoUVSphere)::EmbeddedDeltaSet2D
-  loadmesh_helper("thermo_tie_gcm.obj")
+function loadmesh(meshkey::UnitIcosphere4)::EmbeddedDeltaSet2D
+  loadmesh_helper("UnitIcosphere4.obj")
+end
+
+function loadmesh(meshkey::UnitIcosphere5)::EmbeddedDeltaSet2D
+  loadmesh_helper("UnitIcosphere5.obj")
 end
 
--- test/Decapodes2/meshes.jl
+++ test/Decapodes2/meshes.jl
@@ -9,58 +9,58 @@ include("../../src/Decapodes2/meshes.jl")
 @testset "Meshes" begin
 
 # Unit Icosphere
-unit_icosphere = loadmesh(UnitIcosphere())
-@test nv(unit_icosphere) == 2562
-@test ne(unit_icosphere) == 7680
-@test nparts(unit_icosphere, :Tri) == 5120
-
-# Icosphere at minimum altitude of thermosphere
-thermo_icosphere = loadmesh(ThermoIcosphere())
-@test nv(thermo_icosphere) == 2562
-@test ne(thermo_icosphere) == 7680
-@test nparts(thermo_icosphere, :Tri) == 5120
-
-# Unit UV sphere (TIE GCM grid)
-unit_uvsphere = loadmesh(UnitUVSphere())
-@test nv(unit_uvsphere) == 2522
-@test ne(unit_uvsphere) == 7560
-@test nparts(unit_uvsphere, :Tri) == 5040
-
-# Icosphere at minimum altitude of thermosphere
-thermo_uvsphere = loadmesh(ThermoUVSphere())
-@test nv(thermo_uvsphere) == 2522
-@test ne(thermo_uvsphere) == 7560
-@test nparts(thermo_uvsphere, :Tri) == 5040
-
-
-magnitude = (sqrt ∘ (x -> foldl(+, x*x)))
-
-# The definition of a discretization of a sphere of unspecified radius.
-ρ′ = magnitude(unit_icosphere[:point][begin])
-@test all(isapprox.(magnitude.(unit_icosphere[:point]), ρ′))
-# The definition of a discretization of a sphere of radius ρ.
-ρ = 1
-@test all(isapprox.(magnitude.(unit_icosphere[:point]), ρ))
-
-# The definition of a discretization of a sphere of unspecified radius.
-ρ′ = magnitude(thermo_icosphere[:point][begin])
-@test all(isapprox.(magnitude.(thermo_icosphere[:point]), ρ′))
-ρ = 6371+90
-# The definition of a discretization of a sphere of radius ρ.
-@test all(isapprox.(magnitude.(thermo_icosphere[:point]), ρ))
-
-# The definition of a discretization of a sphere of unspecified radius.
-ρ′ = magnitude(unit_uvsphere[:point][begin])
-@test all(isapprox.(magnitude.(unit_uvsphere[:point]), ρ′))
-# The definition of a discretization of a sphere of radius ρ.
-ρ = 1
-@test all(isapprox.(magnitude.(unit_uvsphere[:point]), ρ))
-
-# The definition of a discretization of a sphere of unspecified radius.
-ρ′ = magnitude(thermo_uvsphere[:point][begin])
-@test all(isapprox.(magnitude.(thermo_uvsphere[:point]), ρ′))
-# The definition of a discretization of a sphere of radius ρ.
-ρ = 6371+90
-@test all(isapprox.(magnitude.(thermo_uvsphere[:point]), ρ))
+#unit_icosphere = loadmesh(UnitIcosphere())
+#@test nv(unit_icosphere) == 2562
+#@test ne(unit_icosphere) == 7680
+#@test nparts(unit_icosphere, :Tri) == 5120
+#
+## Icosphere at minimum altitude of thermosphere
+#thermo_icosphere = loadmesh(ThermoIcosphere())
+#@test nv(thermo_icosphere) == 2562
+#@test ne(thermo_icosphere) == 7680
+#@test nparts(thermo_icosphere, :Tri) == 5120
+#
+## Unit UV sphere (TIE GCM grid)
+#unit_uvsphere = loadmesh(UnitUVSphere())
+#@test nv(unit_uvsphere) == 2522
+#@test ne(unit_uvsphere) == 7560
+#@test nparts(unit_uvsphere, :Tri) == 5040
+#
+## Icosphere at minimum altitude of thermosphere
+#thermo_uvsphere = loadmesh(ThermoUVSphere())
+#@test nv(thermo_uvsphere) == 2522
+#@test ne(thermo_uvsphere) == 7560
+#@test nparts(thermo_uvsphere, :Tri) == 5040
+#
+#
+#magnitude = (sqrt ∘ (x -> foldl(+, x*x)))
+#
+## The definition of a discretization of a sphere of unspecified radius.
+#ρ′ = magnitude(unit_icosphere[:point][begin])
+#@test all(isapprox.(magnitude.(unit_icosphere[:point]), ρ′))
+## The definition of a discretization of a sphere of radius ρ.
+#ρ = 1
+#@test all(isapprox.(magnitude.(unit_icosphere[:point]), ρ))
+#
+## The definition of a discretization of a sphere of unspecified radius.
+#ρ′ = magnitude(thermo_icosphere[:point][begin])
+#@test all(isapprox.(magnitude.(thermo_icosphere[:point]), ρ′))
+#ρ = 6371+90
+## The definition of a discretization of a sphere of radius ρ.
+#@test all(isapprox.(magnitude.(thermo_icosphere[:point]), ρ))
+#
+## The definition of a discretization of a sphere of unspecified radius.
+#ρ′ = magnitude(unit_uvsphere[:point][begin])
+#@test all(isapprox.(magnitude.(unit_uvsphere[:point]), ρ′))
+## The definition of a discretization of a sphere of radius ρ.
+#ρ = 1
+#@test all(isapprox.(magnitude.(unit_uvsphere[:point]), ρ))
+#
+## The definition of a discretization of a sphere of unspecified radius.
+#ρ′ = magnitude(thermo_uvsphere[:point][begin])
+#@test all(isapprox.(magnitude.(thermo_uvsphere[:point]), ρ′))
+## The definition of a discretization of a sphere of radius ρ.
+#ρ = 6371+90
+#@test all(isapprox.(magnitude.(thermo_uvsphere[:point]), ρ))
 
 end
