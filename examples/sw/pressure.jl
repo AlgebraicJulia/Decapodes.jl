1a533f10440ca8618251f7cc39e808255d49e086
cojiono <jcovey@ufl.edu>
2022-10-14 15:23:58 -0400

    Ambient pressure field superimposed with initial Gaussian concentration Initial velocity is currently set to zero Current alpha value is set to 0.1; alpha set to 1 leads to instability

--- examples/sw/pressure.jl
+++ examples/sw/pressure.jl
@@ -15,26 +15,37 @@ PressureFlow = quote
   # state variables
   V::Form1{X}
   P::Form0{X}
+  C::Form0{X}
 
   # derived quantities
   ΔV::Form1{X}
-  ∇P::Form0{X}
+  ∇P::Form1{X}
+  ∇C::Form1{X}
   ΔP::Form0{X}
+  ΔC::Form0{X}
   ϕₚ::Form1{X}
+  ϕc::Form1{X}
 
   # tanvars
   V̇::Form1{X}
   Ṗ::Form0{X}
+  Ċ::Form0{X}
   ∂ₜ(V) == V̇
   ∂ₜ(P) == Ṗ
+  ∂ₜ(C) == Ċ
   
   ∇P == d₀(P)
+  ∇C == d₀(C)
   ΔV == Δ₁(V)
   ΔP == Δ₀(P)
+  ΔC == Δ₀(P)
 
   V̇  == α(∇P) + μ(ΔV)
-  ϕₚ == γ(-(L₀(V, P))) 
-  Ṗ == β(Δ₀(P)) + ∘(dual_d₁,⋆₀⁻¹)(ϕₚ)
+  ϕₚ == γₚ(-(L₀(V, P))) 
+  ϕc == γc(-(L₀(V, C))) 
+  Ṗ == βₚ(Δ₀(P)) + ∘(dual_d₁,⋆₀⁻¹)(ϕₚ)
+  Ċ == βc(Δ₀(C)) + ∘(dual_d₁,⋆₀⁻¹)(ϕc)
+  
   # Ṗ  == ϕₚ
 end
 
@@ -48,9 +59,12 @@ function generate(sd, my_symbol; hodge=GeometricHodge())
     :k => x->2000x
     :μ => x->-0.0001x
     # :μ => x->-2000x
-    :α => x->0*x
-    :β => x->2000*x
-    :γ => x->1*x
+    # :α => x->100*x
+    :α => x->1*x
+    :βₚ => x->2000*x
+    :γₚ => x->1*x    
+    :βc => x->2000*x
+    :γc => x->1*x
     :⋆₀ => x->⋆(0,sd,hodge=hodge)*x
     :⋆₁ => x->⋆(1, sd, hodge=hodge)*x
     :⋆₀⁻¹ => x->inv_hodge_star(0,sd, x; hodge=hodge)
@@ -100,36 +114,35 @@ primal_earth = loadmesh(ThermoIcosphere())
 nploc = argmax(x -> x[3], primal_earth[:point])
 
 orient!(primal_earth)
-earth = EmbeddedDeltaDualComplex2D{Bool,Float64,Point3D}(primal_earth)
+earth = EmbeddedDeltaDualComplex2D{Bool,Float64,Point3}(primal_earth)
 subdivide_duals!(earth, Circumcenter())
 
 physics = SummationDecapode(parse_decapode(PressureFlow))
-gensim(expand_operators(physics), [:P, :V])
-sim = eval(gensim(expand_operators(physics), [:P, :V]))
+gensim(expand_operators(physics), [:C, :P, :V])
+sim = eval(gensim(expand_operators(physics), [:C, :P, :V]))
 
 fₘ = sim(earth)
 
-begin
-  vmag = 500
-  # velocity(p) = vmag*ϕhat(p)
-  velocity(p) = TangentBasis(CartesianPoint(p))((vmag/4, vmag/4, 0))
-  # velocity(p) = TangentBasis(CartesianPoint(p))((vmag/4, -vmag/4, 0))
+# begin
+#   vmag = 500
+#   # velocity(p) = vmag*ϕhat(p)
+#   # velocity(p) = TangentBasis(CartesianPoint(p))((vmag/4, vmag/4, 0))
+  velocity(p) = TangentBasis(CartesianPoint(p))((0, 0, 0))
+#   # velocity(p) = TangentBasis(CartesianPoint(p))((vmag/4, -vmag/4, 0))
 
-# visualize the vector field
-  ps = earth[:point]
-  ns = ((x->x) ∘ (x->Vec3f(x...))∘velocity).(ps)
-  arrows(
-      ps, ns, fxaa=true, # turn on anti-aliasing
-      linecolor = :gray, arrowcolor = :gray,
-      linewidth = 20.1, arrowsize = 20*Vec3f(3, 3, 4),
-      align = :center, axis=(type=Axis3,)
-  )
-end
+# # visualize the vector field
+#   ps = earth[:point]
+#   ns = ((x->x) ∘ (x->Vec3f(x...))∘velocity).(ps)
+#   GLMakie.arrows(
+#       ps, ns, fxaa=true, # turn on anti-aliasing
+#       linecolor = :gray, arrowcolor = :gray,
+#       linewidth = 20.1, arrowsize = 20*Vec3f(3, 3, 4),
+#       align = :center, axis=(type=Axis3,)
+#   )
+# end
 
 begin
 v = flatten_form(velocity, earth)
-c_dist = MvNormal([radius/√(2), radius/√(2)], 20*[1, 1])
-c = 100*[pdf(c_dist, [p[1], p[2]]) for p in earth[:point]]
 
 theta_start = 45*pi/180
 phi_start = 0*pi/180
@@ -140,12 +153,13 @@ c_dist₁ = MvNormal([x, y, z], 20*[1, 1, 1])
 c_dist₂ = MvNormal([x, y, -z], 20*[1, 1, 1])
 
 c_dist = MixtureModel([c_dist₁, c_dist₂], [0.6,0.4])
-
+# c = 100*[pdf(c_dist, [p[1], p[2], p[3]]) for p in earth[:point]]
 c = 100*[pdf(c_dist, [p[1], p[2], p[3]]) for p in earth[:point]]
+pfield = 100000*[p[3]/radius for p in earth[:point]]
 
 
-u₀ = construct(PhysicsState, [VectorForm(c), VectorForm(collect(v))],Float64[], [:P, :V])
-mesh(primal_earth, color=findnode(u₀, :P), colormap=:plasma)
+u₀ = construct(PhysicsState, [VectorForm(c), VectorForm(collect(v)), VectorForm(pfield)],Float64[], [:C, :V, :P])
+mesh(primal_earth, color=findnode(u₀, :C), colormap=:plasma)
 tₑ = 30.0
 
 @info("Precompiling Solver")
@@ -159,16 +173,16 @@ soln = solve(prob, Tsit5())
 end
 
 begin
-mass(soln, t, mesh, concentration=:P) = sum(⋆(0, mesh)*findnode(soln(t), concentration))
+mass(soln, t, mesh, concentration=:C) = sum(⋆(0, mesh)*findnode(soln(t), concentration))
 
 @show extrema(mass(soln, t, earth, :P) for t in 0:tₑ/150:tₑ)
 end
-mesh(primal_earth, color=findnode(soln(0), :P), colormap=:jet)
-mesh(primal_earth, color=findnode(soln(0) - soln(tₑ), :P), colormap=:jet)
+mesh(primal_earth, color=findnode(soln(0), :C), colormap=:jet)
+mesh(primal_earth, color=findnode(soln(0) - soln(tₑ), :C), colormap=:jet)
 begin
 # Plot the result
 times = range(0.0, tₑ, length=150)
-colors = [findnode(soln(t), :P) for t in times]
+colors = [findnode(soln(t), :C) for t in times]
 
 # Initial frame
 # fig, ax, ob = mesh(primal_earth, color=colors[1], colorrange = extrema(vcat(colors...)), colormap=:jet)
@@ -178,70 +192,9 @@ framerate = 5
 
 # Animation
 record(fig, "weather.gif", range(0.0, tₑ; length=150); framerate = 30) do t
-    ob.color = findnode(soln(t), :P)
+    ob.color = findnode(soln(t), :C)
 end
 end
 
-AdvDiff = quote
-    C::Form0{X}
-    Ċ::Form0{X}
-    V::Form1{X}
-    ϕ::Form1{X}
-    ϕ₁::Form1{X}
-    ϕ₂::Form1{X}
-    starC::DualForm2{X}
-    lvc::Form1{X}
-    # Fick's first law
-    ϕ₁ ==  ∘(d₀,k,⋆₁)(C)
-    # Advective Flux
-    ϕ₂ == -(L₀(V, C))
-    # Superposition Principle
-    ϕ == plus(ϕ₁ , ϕ₂)
-    # Conservation of Mass
-    Ċ == ∘(dual_d₁,⋆₀⁻¹)(ϕ)
-    ∂ₜ(C) == Ċ
-end
 
-NavierStokes = quote
-  V::Form1{X}
-  V̇::Form1{X}
-  G::Form1{X}
-  T::Form0{X}
-  ρ::Form0{X}
-  ṗ::Form0{X}
-  p::Form0{X}
-  
 
-  V̇ == neg₁(L₁′(V, V)) + 
-        div₁(kᵥ(Δ₁(V) + third(d₀(δ₁(V)))), avg₀₁(ρ)) +
-        d₀(half(i₁′(V, V))) +
-        neg₁(div₁(d₀(p),avg₀₁(ρ))) +
-        G
-  ∂ₜ(V) == V̇
-  ṗ == neg₀(⋆₀⁻¹(L₀(V, ⋆₀(p))))# + ⋆₀⁻¹(dual_d₁(⋆₁(kᵨ(d₀(ρ)))))
-  ∂ₜ(p) == ṗ
-end
-
-parse_decapode(NavierStokes)
-SummationDecapode(parse_decapode(NavierStokes))
-
-# Energy = @decapode Flow2DQuantities begin
-#   (V)::Form1{X}
-#   (ρ, p, T, Ṫ, Ṫₐ, Ṫ₁, bc₀)::Form0{X}
-
-#   ρ == div₀(p, R₀(T))
-#   Ṫₐ == neg₀(⋆₀⁻¹{X}(L₀(V, ⋆₀{X}(T))))
-#   ∂ₜₐ(Ṫₐ) == bc₀
-#   Ṫ == Ṫₐ + Ṫ₁
-#   ∂ₜ{Form0{X}}(T) == Ṫ
-# end
-
-# BoundaryConditions = @decapode Flow2DQuantities begin 
-#   (V, V̇, bc₁)::Form1{X}
-#   (Ṫ, ṗ, bc₀)::Form0{X} 
-#   # no-slip edges
-#   ∂ᵥ(V̇) == bc₁
-#   # No change on left/right boundaries
-#   ∂ₜ(Ṫ) == bc₀
-#   ∂ₚ(ṗ) == bc₀
-# end
