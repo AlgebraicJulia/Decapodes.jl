57ede0ba7db41c92e49a63f6d54d92b7d48e466f
Luke Morris <lukelukemorrismorris@gmail.com>
2022-11-07 14:38:40 -0500

    Add visualization tests

--- test/Decapodes2/visualization.jl
+++ test/Decapodes2/visualization.jl
@@ -1,3 +1,4 @@
+using Test
 using Decapodes
 using Catlab.Graphics
 using Catlab.Graphics.Graphviz
@@ -13,7 +14,32 @@ DecaTest = quote
   end
   
 Test1 = SummationDecapode(parse_decapode(DecaTest))
-to_graphviz(Test1)
+t1 = to_graphviz(Test1)
+@test Graphviz.filter_statements(t1, Graphviz.Edge, :label) == ["k",
+  "p",
+  "+",
+  "+",
+  "+"]
+@test Graphviz.filter_statements(t1, Graphviz.Node, :label) == [
+  "A:Ω₀",
+  "B:Ω₀",
+  "C:Ω₀",
+  "D:Ω₀",
+  "•1:Ω•",
+  "sum_1:Ω•",
+  "•2:Ω•",
+  "sum_2:Ω•",
+  "",
+  "",
+  "Σ1",
+  "Σ2",
+  "Σ3" ]
+@test Graphviz.filter_statements(t1, Graphviz.Node, :shape) == [
+  "none",
+  "none",
+  "circle",
+  "circle",
+  "circle"]
 
 DecaTest2 = quote
   A::Form0{X}
@@ -27,5 +53,12 @@ DecaTest2 = quote
 end
 
 Test2 = SummationDecapode(parse_decapode(DecaTest2))
-to_graphviz(Test2)
-to_graphviz(Test2, isDirected = false)
+t2 = to_graphviz(Test2)
+@test Graphviz.filter_statements(t2, Graphviz.Edge, :label) == ["∂ₜ", "π₁", "π₂", "k", "π₁", "π₂", "p", "+"]
+@test Graphviz.filter_statements(t2, Graphviz.Node, :label) == ["A:Ω₀", "B:Ω₀", "C:Ω₀", "D:Ω₀", "Ḋ:Ω•", "•1:Ω•", "•2:Ω•", "", "", "Ω₀×Ω₀", "Ω₀×Ω₀", "Σ1"]
+@test Graphviz.filter_statements(t2, Graphviz.Node, :shape) == ["none", "none", "rectangle", "rectangle", "circle"]
+
+t2_undirected = to_graphviz(Test2, isDirected = false)
+@test Graphviz.filter_statements(t2_undirected, Graphviz.Edge, :label) == ["∂ₜ", "π₁", "π₂", "k", "π₁", "π₂", "p", "+"]
+@test Graphviz.filter_statements(t2_undirected, Graphviz.Node, :label) == ["A:Ω₀", "B:Ω₀", "C:Ω₀", "D:Ω₀", "Ḋ:Ω•", "•1:Ω•", "•2:Ω•", "Ω₀×Ω₀", "Ω₀×Ω₀", "Σ1"]
+@test Graphviz.filter_statements(t2_undirected, Graphviz.Node, :shape) == ["rectangle", "rectangle", "circle"]
