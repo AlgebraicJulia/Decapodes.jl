decf186803490adbe09b36252c3ac968c266e290
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2023-10-12 13:33:00 -0400

    Use Op2 to encode boundaries

--- src/collages.jl
+++ src/collages.jl
@@ -9,17 +9,28 @@ function collate(equations, boundaries, uwd, symbols)
 
   f = SummationDecapode{Any, Any, Symbol}()
   # TODO: Double-check
-  copy_parts!(f, equations, (:Var, :TVar, :Op1, :Op2, :Σ, :Summand, :Sum))
+  copy_parts!(f, equations, (:Var, :TVar, :Op1, :Op2, :Σ, :Summand))
+
+  # TODO: Throw an error if the user tries to use a boundary value differential
+  # form that is of a different type of the thing that we are applying the bound
+  # to. i.e. Form1 but target is a Form0.
 
   # TODO: This sets restrictions as Op1s. They are actually Op2s. i.e. Use `bv`.
   for b in boxes(uwd)
-    ps = incident(uwd, b :box)
+    ps = incident(uwd, b, :box)
     ev = first(ps)
     bv = last(ps)
-    var = incident(f, ev, :name)
+    en_key = uwd[junction(uwd, ev), :variable]
+    bn_key = uwd[junction(uwd, bv), :variable]
+    en = symbols[en_key]
+    bn = symbols[bn_key] # This will be used when we do error-checking on types.
+    println(en_key, en)
+    var = only(incident(f, en, :name))
     b_var = add_part!(f, :Var, type=f[var, :type], name=f[var, :name])
     f[var, :name] = Symbol("r_" * string(f[var, :name]))
-    add_part!(f, :Op1, src=b_var, tgt=var, op1=uwd[b, :name])
+    #add_part!(f, :Op1, src=b_var, tgt=var, op1=uwd[b, :name])
+    s_var = add_part!(f, :Var, type=f[var, :type], name=bn)
+    add_part!(f, :Op2, proj1=b_var, proj2=s_var, res=var, op2=uwd[b, :name])
   end
 
   f
--- test/collages.jl
+++ test/collages.jl
@@ -1,10 +1,6 @@
 using Test
 using Decapodes
 using Catlab
-using Catlab.WiringDiagrams
-using Catlab.Programs
-using Catlab.CategoricalAlgebra
-
 
 # TODO: Special names for morphisms that match a method of grabbing boundary
 # simplices.
@@ -18,37 +14,54 @@ using Catlab.CategoricalAlgebra
 
 # Test simple boundary masks.
 DiffusionDynamics = @decapode begin
-  C::Form0
-  ∂ₜ(C) == ∘(d,⋆,d,⋆)(C)
+  K::Form0
+  ∂ₜ(K) == ∘(d,⋆,d,⋆)(K)
 end
 DiffusionBoundaries = @decapode begin
-  (Cb1, Cb2, Zero)::Form0
+  (Kb1, Kb2, Null)::Form0
 end
 
 DiffusionMorphism = @relation () begin
-  rb1(C, Cb1)
-  rb2(C, Cb2)
+  rb1_leftwall(C, Cb1)
+  rb2_rightwall(C, Cb2)
   rb3(Ċ, Zero)
 end
 
-DiffusionCollage = collate(DiffusionMorphism,
-  DiffusionDynamics, DiffusionBoundaries, [
-  (:C, :Cb1),
-  (:C, :Cb2),
-  (:Ċ, :Zero)])
+DiffusionCollage = collate(
+  DiffusionDynamics,
+  DiffusionBoundaries,
+  DiffusionMorphism,
+  Dict(
+    :C => :K,
+    :Ċ => :K̇,
+    :Cb1 => :Kb1,
+    :Cb2 => :Kb2,
+    :Zero => :Null))
+#to_graphviz(DiffusionCollage)
+
+@test DiffusionCollage == @acset SummationDecapode{Any, Any, Symbol} begin
+  Var = 8
+  TVar = 1
+  Op1 = 2
+  Op2 = 3
+  src  = [1, 1]
+  tgt  = [2, 2]
+  proj1  = [3, 5, 7]
+  proj2  = [4, 6, 8]
+  res  = [1, 3, 2]
+  incl  = [2]
+  op1  = Any[:∂ₜ, [:d, :⋆, :d, :⋆]]
+  op2  = [:rb1_leftwall, :rb2_rightwall, :rb3]
+  type  = [:Form0, :infer, :Form0, :Form0, :Form0, :Form0, :infer, :infer]
+  name  = [:r_K, :r_K̇, :r_K, :Kb1, :K, :Kb2, :K̇, :Null]
+end
 
 # Note: Since the order does not matter in which rb1 and rb2 are applied, it
 # seems informal to state that one goes before the other.
 # It might be better to provide a semantics for incident edges a la:
 #Diffusion = @decapode begin
 #  C::Form0
-#  #∂ₜ(C) == rb3(∘(d,⋆,d,⋆)(rb1(C)))
-#  #∂ₜ(C) == rb3(∘(d,⋆,d,⋆)(rb2(C)))
+#  ∂ₜ(C) == rb3(∘(d,⋆,d,⋆)(rb1(C)))
+#  ∂ₜ(C) == rb3(∘(d,⋆,d,⋆)(rb2(C)))
 #end
-
-Diffusion = @decapode begin
-  C::Form0
-  ∂ₜ(C) == rb3(∘(d,⋆,d,⋆)(rb2(rb1(C))))
-end
-
-@test_fails is_isomorphic(DiffusionCollage, Diffusion)
+# Such a technique would preserve the technical definition of "collage".
