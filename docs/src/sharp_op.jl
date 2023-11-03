c85cdc5136964d04fe97438430a49be5b4ad38d2
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2023-11-03 11:38:17 -0700

    Update docs for sharp deps

--- docs/src/sharp_op.jl
+++ docs/src/sharp_op.jl
@@ -1,12 +1,15 @@
 # TODO: Upstream these operators to CombinatorialSpaces.jl:
 # https://github.com/AlgebraicJulia/CombinatorialSpaces.jl/pull/59/files
 
-""" Divided weighted normals by | σⁿ | .
-This weighting is that used in equation 5.8.1 from Hirani.
-See Hirani §5.8.
-"""
-♯_denominator(s::AbstractDeltaDualComplex2D, _::Int, t::Int, ::DiscreteSharp) =
-  volume(2,s,t)
+using SparseArrays
+using StaticArrays
+
+#""" Divided weighted normals by | σⁿ | .
+#This weighting is that used in equation 5.8.1 from Hirani.
+#See Hirani §5.8.
+#"""
+#♯_denominator(s::AbstractDeltaDualComplex2D, _::Int, t::Int) =
+#  volume(2,s,t)
 
 """ Divided weighted normals by | ⋆v | .
 This weighting is NOT that of equation 5.8.1, but a different weighting scheme.
@@ -15,7 +18,7 @@ may be what Hirani intended, and perhaps the denominator | σⁿ | in that equat
 is either a mistake or clerical error.
 See Hirani §5.8.
 """
-♯_denominator(s::AbstractDeltaDualComplex2D, v::Int, _::Int, ::AltPPSharp) =
+♯_denominator(s::AbstractDeltaDualComplex2D, v::Int, _::Int) =
   sum(dual_volume(2,s, elementary_duals(0,s,v)))
 
 """ Find a vector orthogonal to e pointing into the triangle shared with v.
@@ -29,38 +32,26 @@ end
 
 function ♯_assign!(♯_mat::AbstractSparseMatrix, s::AbstractDeltaDualComplex2D, 
   v₀::Int, _::Int, t::Int, i::Int, tri_edges::SVector{3, Int}, tri_center::Int,
-  out_vec, DS::DiscreteSharp)
+  out_vec, DS::CombinatorialSpaces.DiscreteExteriorCalculus.DiscreteSharp)
   for e in deleteat(tri_edges, i)
     v, sgn = src(s,e) == v₀ ? (tgt(s,e), -1) : (src(s,e), +1)
     # | ⋆vₓ ∩ σⁿ |
     dual_area = sum(dual_volume(2,s,d) for d in elementary_duals(0,s,v)
                     if s[s[d, :D_∂e0], :D_∂v0] == tri_center)
-    area = ♯_denominator(s, v, t, DS)
+    area = ♯_denominator(s, v, t)
     ♯_mat[v,e] += sgn * sign(1,s,e) * (dual_area / area) * out_vec
   end
 end
 
-function ♯_assign!(♯_mat::AbstractSparseMatrix, s::AbstractDeltaDualComplex2D, 
-  _::Int, e₀::Int, t::Int, _::Int, _::SVector{3, Int}, tri_center::Int,
-  out_vec, DS::DesbrunSharp)
-  for v in edge_vertices(s, e₀)
-    sgn = v == tgt(s,e₀) ? -1 : +1
-    # | ⋆vₓ ∩ σⁿ |
-    dual_area = sum(dual_volume(2,s,d) for d in elementary_duals(0,s,v)
-                    if s[s[d, :D_∂e0], :D_∂v0] == tri_center)
-    area = ♯_denominator(s, v, t, DS)
-    ♯_mat[v,e₀] += sgn * sign(1,s,e₀) * (dual_area / area) * out_vec
-  end
-end
-
-function ♯_mat(s::AbstractDeltaDualComplex2D, DS::DiscreteSharp)
+function ♯_mat(s::AbstractDeltaDualComplex2D, DS::CombinatorialSpaces.DiscreteExteriorCalculus.DiscreteSharp)
   ♯_mat = spzeros(attrtype_type(s, :Point), (nv(s), ne(s)))
   for t in triangles(s)
     tri_center, tri_edges = triangle_center(s,t), triangle_edges(s,t)
     for (i, (v₀, e₀)) in enumerate(zip(triangle_vertices(s,t), tri_edges))
       out_vec = get_orthogonal_vector(s, v₀, e₀)
       h = norm(out_vec)
-      out_vec /= DS == DesbrunSharp() ? h : h^2
+      #out_vec /= DS == DesbrunSharp() ? h : h^2
+      out_vec /= h^2
       ♯_assign!(♯_mat, s, v₀, e₀, t, i, tri_edges, tri_center, out_vec, DS)
     end
   end
