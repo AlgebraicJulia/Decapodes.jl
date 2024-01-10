82901bda6e8de6221df1c9a8af63741ef7365d9e
Matt Cuffaro <cuffaro.matt@gmail.com>
2024-01-10 08:46:43 -0500

    continuing work on this feature

--- src/resourceloading.jl
+++ src/resourceloading.jl
@@ -1,20 +1,31 @@
 # 
 
-struct Resource
-    path::Vector{String}
-    type::Type
+struct Resource{T}
+  path::Vector{String}
 end 
 
 abstract type ResourceProvider end
 
-struct DirCsvsBackend <: ResourceProvider
-    path::String
+struct DirSsfsBackend <: ResourceProvider
+  dir::String
 end 
 
 struct HDF5Backend <: ResourceProvider
-    path::String
+  path::String
 end 
 
-load(p::ResourceProvider, r::Resource)
+# csv and stuff
 
-save(p::ResourceProvider, r::Resource, x)
+load(p::ResourceProvider, r::Resource) = 
+
+function load(p::DirSsfsBackend, r::Resource{Vector{Float64}})
+  # TODO safe!
+  path = p.dir * join(r.path, "/")
+  content = read(path, String)
+  map(s -> parse(Float64, s), split(content, " "))
+end
+
+function save(p::DirSsfsBackend, r::Resource{Vector{Float64}}, x::Vector{Float64})
+  file = join(x, " ")
+  write(p.path * join(r.path, "/"), file)
+end
