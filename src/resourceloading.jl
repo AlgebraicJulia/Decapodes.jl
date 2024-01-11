76475452af8e7380788171cc2d8b5c18d6df3248
Matt <cuffaro.matt@gmail.com>
2024-01-11 15:48:34 -0500

    working on resource loading code

--- src/resourceloading.jl
+++ src/resourceloading.jl
@@ -1,31 +1,73 @@
-# 
+using HDF5
+using Mat
 
 struct Resource{T}
-  path::Vector{String}
+  path::String
 end 
 
 abstract type ResourceProvider end
 
+
+## SPACE-DELIMITED FILES
 struct DirSsfsBackend <: ResourceProvider
   dir::String
 end 
 
-struct HDF5Backend <: ResourceProvider
-  path::String
-end 
-
-# csv and stuff
-
-load(p::ResourceProvider, r::Resource) = 
-
 function load(p::DirSsfsBackend, r::Resource{Vector{Float64}})
-  # TODO safe!
-  path = p.dir * join(r.path, "/")
+  path = joinpath(p.dir, r.path) 
   content = read(path, String)
   map(s -> parse(Float64, s), split(content, " "))
 end
 
 function save(p::DirSsfsBackend, r::Resource{Vector{Float64}}, x::Vector{Float64})
   file = join(x, " ")
-  write(p.path * join(r.path, "/"), file)
+  write(joinpath(p.dir, r.path), file)
 end
+
+## CHARACTER-DELIMITED FILES
+struct DirCdfsBackend <: ResourceProvider
+  dir::String
+  char::String
+end
+
+function load(p::DirCdfsBackend, r::Resource{Vector{Float64}})
+  path = joinpath(p.dir, r.path) 
+  content = h5open(path, "cw")
+  map(s -> parse(Float64, s), split(content, r.char))
+end
+
+function save(p::DirCdfsBackend, r::Resource{Vector{Float64}}, x::Vector{Float64})
+  file = join(x, r.char)
+  write(joinpath(p.dir, r.path), file)
+end
+
+
+## HDF5 FILES
+struct HDF5Backend <: ResourceProvider
+  path::String
+end 
+## MAT
+struct MatBackend <: ResourceProvider
+  path::String
+end
+
+# load(p::ResourceProvider, r::Resource) = 
+
+
+
+
+function load(p::HDF5Backend, r::Resource{Vector{Float64}})
+  path = joinpath(p.dir, r.path)
+  content = read(path, String)
+
+end
+#
+
+## MAT
+function load(p::MatBackend, r::Resource{Vector{Float64}})
+  path = joinpath(p.dir, r.path)
+  file = MAT.matread(path)
+end
+
+p=DirSsfsBackend("/home/you/Documents/UFAJ/AlgebraicJulia/Decapodes.jl/src/")
+r=Resource{Vector{Float64}}("testssf.txt")
