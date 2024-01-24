4148b1f93167d8d630e8c0548c680278bca2e43c
Matt <cuffaro.matt@gmail.com>
2024-01-24 11:43:18 -0500

    changed jobscript for multithreading, 64 cores

--- .buildkite/jobscript.sh
+++ .buildkite/jobscript.sh
@@ -8,4 +8,4 @@ echo "Running Tests..."
 julia --project -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia -t 16 --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
+julia -t 64 --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
