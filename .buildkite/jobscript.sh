c2ec4df101c6a658c6d054d884391b840e32cb53
Matt <cuffaro.matt@gmail.com>
2024-01-24 11:43:18 -0500

    changed jobscript for multithreading

--- .buildkite/jobscript.sh
+++ .buildkite/jobscript.sh
@@ -2,12 +2,10 @@
 
 pwd; hostname; date
 
-echo "Hello!"
-
 module load julia
 
 echo "Running Tests..."
 julia --project -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
+julia -t 16 --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
