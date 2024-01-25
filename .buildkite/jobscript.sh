8e6fad923e7cd7607d8d644ac7ffb44433c0aca1
Matt <cuffaro.matt@gmail.com>
2024-01-25 12:14:33 -0500

    32 cores

--- .buildkite/jobscript.sh
+++ .buildkite/jobscript.sh
@@ -5,7 +5,7 @@ pwd; hostname; date
 module load julia
 
 echo "Running Tests..."
-julia -t 32 --project -e 'using Pkg; Pkg.status(); Pkg.test()'
+julia --project -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
 julia -t 32 --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
