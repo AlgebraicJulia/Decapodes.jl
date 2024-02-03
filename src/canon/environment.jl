3d1fb194a828080018b7906f01c4efcd6cc3ee2a
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2024-02-03 16:10:01 -0500

    Cite environment Decapodes, and update Halfar

--- src/canon/environment.jl
+++ src/canon/environment.jl
@@ -3,7 +3,7 @@ using DiagrammaticEquations.Deca
 
 @docapode(GlensLaw
   ,"https://www.google.com"
-  ,""
+  ,"Nye, J. F. (1957). The Distribution of Stress and Velocity in Glaciers and Ice-Sheets. Proceedings of the Royal Society of London. Series A, Mathematical and Physical Sciences, 239(1216), 113–133. http://www.jstor.org/stable/100184"
   ,glen
   ,begin 
     Γ::Form1
@@ -15,14 +15,16 @@ using DiagrammaticEquations.Deca
 
 @docapode(halfar_eq2
   ,"https://www.google.com"
-  ,"Klausmeier, CA. “Regular and irregular patterns in semiarid vegetation.” Science (New York, N.Y.) vol. 284,5421 (1999): 1826-8. doi:10.1126/science.284.5421.1826"
+  ,"Halfar, P. (1981), On the dynamics of the ice sheets, J. Geophys. Res., 86(C11), 11065–11072, doi:10.1029/JC086iC11p11065"
   ,halfar_eq2
-  ,begin
+  ,beginhalfar_eq2 = @decapode begin
     h::Form0
     Γ::Form1
     n::Constant
 
-    ∂ₜ(h) == ∘(⋆, d, ⋆)(Γ * d(h) ∧ mag(♯(d(h)))^(n-1) ∧ (h^(n+2)))
+    ∂ₜ(h) == ∘(⋆, d, ⋆)(Γ  * d(h) ∧ (mag(♯(d(h)))^(n-1)) ∧ (h^(n+2)))
+end
+
   end
 )
 
@@ -52,7 +54,7 @@ using DiagrammaticEquations.Deca
 
 @docapode(Lejeune
   ,"https://www.google.com"
-  ,"desc"
+  ,"Lejeune, O., & Tlidi, M. (1999). A Model for the Explanation of Vegetation Stripes (Tiger Bush). Journal of Vegetation Science, 10(2), 201–208. https://doi.org/10.2307/3237141"
   ,lejeune
   ,begin
     ρ::Form0
