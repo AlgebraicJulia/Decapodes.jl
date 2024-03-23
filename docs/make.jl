b14dcb6b3ba34368ef77b0486b0598e33f534491
Matt <cuffaro.matt@gmail.com>
2024-03-23 10:38:43 -0400

    removed Manifest.toml and removed dead code

--- docs/make.jl
+++ docs/make.jl
@@ -34,7 +34,7 @@ for (root, dirs, files) in walkdir(literate_dir)
 end
 @info "Completed literate"
 
-pages = Vector{Pair{String, Union{String, Vector{String}}}}()
+pages = Any[] 
 push!(pages, "Decapodes.jl"      => "index.md")
 push!(pages, "Overview"          => "overview.md")
 push!(pages, "Equations"         => "equations.md")
