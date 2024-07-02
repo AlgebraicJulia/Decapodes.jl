b353bc9565c4cd0c6ea8531f99759efe4f285e3f
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2024-07-02 09:17:18 -0400

    Heat benchmarks

--- test/benchmarks/Heat.jl
+++ test/benchmarks/Heat.jl
@@ -1,4 +1,4 @@
-function setup_heat_benchmark(config)
+function setup_benchmark(config, ::Val{:Heat})
   Heat = @decapode begin
       U::Form0
       ∂ₜ(U) == 100 * Δ(U)
@@ -7,20 +7,23 @@ function setup_heat_benchmark(config)
   eval(gensim(Heat, code_target = config.code_target, stateeltype = config.float_type))
 end
 
-function create_heat_mesh(config)
-  s = triangulated_grid(100, 100, 1, 1, config.point_type)
-  sd = EmbeddedDeltaDualComplex2D{Bool, config.float_type, config.point_type}(s)
+function create_mesh(config, ::Val{:Heat})
+  s = triangulated_grid(config.mesh_size, config.mesh_size, config.mesh_resolution, config.mesh_resolution, config.point_type{config.float_type})
+  sd = EmbeddedDeltaDualComplex2D{Bool, config.float_type, config.point_type{config.float_type}}(s)
   subdivide_duals!(sd, config.center_type)
 
   U = map(sd[:point]) do (x,_)
     return x
+  end
+  if config.code_target isa CUDATarget
+    U = CuArray(U)
   end
   u₀ = ComponentArray(U=U)
 
   (sd, u₀)
 end
 
-function create_heat_simulate(config, sd, simulate)
+function create_simulate(config, sd, simulate, ::Val{:Heat})
   # hodge_type = @match center_type begin
   #   ::Circumcenter => DiagonalHodge()
   #   ::Barycenter => GeometricHodge()
@@ -29,10 +32,15 @@ function create_heat_simulate(config, sd, simulate)
   simulate(sd, nothing, config.hodge_type)
 end
 
-function run_heat_simulation(config, fm, u0)
-  tₑ = config.timeto_execute
+function run_simulation(config, fm, u0, ::Val{:Heat})
+  tₑ = config.float_type(config.timeto_execute)
   prob = ODEProblem(fm, u0, (0, tₑ), ())
 
   soln = solve(prob, Tsit5())
-  soln.stats
+  
+  open("decapode_results_log.txt", "a") do f
+    write(f, string(config)*"\n")
+    write(f, string(soln.stats)*"\n\n")
+  end
+
 end
--- test/benchmarks/benchmarks.jl
+++ test/benchmarks/benchmarks.jl
@@ -10,13 +10,18 @@ using MLStyle
 using ComponentArrays
 using OrdinaryDiffEq
 using GeometryBasics: Point2, Point3
+using CUDA, CUDA.CUSPARSE
 
-struct Config
+struct BenchConfig
+  name::String
+
   float_type::DataType
-  point_type::DataType
+  point_type
   center_type
-
   hodge_type
+
+  mesh_size
+  mesh_resolution
 
   code_target
   timeto_execute::AbstractFloat
@@ -29,37 +34,41 @@ end
 # 4. Run should take the simulation code and initial conditions and run the solve,
 # write the stats to a log
 include("Heat.jl")
-
-heat_config = Config(Float64, Point3{Float64}, Circumcenter(), DiagonalHodge(), CPUTarget(), 11.5)
-
 deca_sim_suite = BenchmarkGroup()
 
-sim = setup_heat_benchmark(heat_config)
-sd, u0 = create_heat_mesh(heat_config)
-fm = create_heat_simulate(heat_config, sd, sim)
-res = run_heat_simulation(heat_config, fm, u0)
-
-deca_sim_suite["Heat"]["Setup"] = @benchmarkable setup_heat_benchmark(heat_config)
-deca_sim_suite["Heat"]["Mesh"] = @benchmarkable create_heat_mesh(heat_config)
-deca_sim_suite["Heat"]["Simulate"] = @benchmarkable create_heat_simulate(heat_config, sd, sim)
-deca_sim_suite["Heat"]["Solve"] = @benchmarkable run_heat_simulation(heat_config, fm, u0)
-
-@info "Simulation Benchmarks "
-
-deca_sim_results = run(deca_sim_suite, verbose = true, seconds = 1)
-
-for sim in sort(collect(keys(deca_sim_results)))
-    test = median(deca_sim_results[sim])
-
-    println("Simulation: $sim")
-    for k in sort(collect(keys(test)))
-        t = test[k].time / 1e6
-        m = test[k].memory / 1e6
-        println("Portion: $k, [$t ms, $m MB]")
-    end
-    println("----------------------------------------------------------------")
+open("decapode_results_log.txt", "w") do f
+  write(f, "Second to last number is count of accepted steps.\n")
 end
 
+for target in [CPUTarget(), CUDATarget()]
+  for res in [1, 0.8, 0.5, 0.4, 0.2]
+    config = BenchConfig("Heat", Float64, Point3, Circumcenter(), DiagonalHodge(), 100, res, target, 11.5)
+    dispatch = Val(Symbol(config.name))
+
+    sim = setup_benchmark(config, dispatch);
+    sd, u0 = create_mesh(config, dispatch);
+    fm = create_simulate(config, sd, sim, dispatch);
+    
+    deca_sim_suite[config.name][config.code_target]["Res: $(config.mesh_resolution)"]["Setup"] = @benchmarkable setup_benchmark($config, $dispatch) gctrial=true
+    deca_sim_suite[config.name][config.code_target]["Res: $(config.mesh_resolution)"]["Mesh"] = @benchmarkable create_mesh($config, $dispatch) gcsample=true
+    deca_sim_suite[config.name][config.code_target]["Res: $(config.mesh_resolution)"]["Simulate"] = @benchmarkable create_simulate($config, $sd, $sim, $dispatch) gctrial=true
+    deca_sim_suite[config.name][config.code_target]["Res: $(config.mesh_resolution)"]["Solve"] = @benchmarkable run_simulation($config, $fm, $u0, $dispatch) gcsample=true
+  end
+end
+
+@info "Tuning benchmarks"
+tune!(deca_sim_suite)
+
+@info "Saving parameters"
+BenchmarkTools.save("decapode_bench_params.json", params(deca_sim_suite));
+
+@info "Running benchmarks"
+deca_sim_results = run(deca_sim_suite, verbose = true)
+@info "Done running benchmarks"
+
+@info "Saving results"
+BenchmarkTools.save("decapode_results.json", deca_sim_results)
+# BenchmarkTools.load("decapode_results.json")
 end
 
 # Gensim
