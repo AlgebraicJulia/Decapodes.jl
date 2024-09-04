c19742c51f7811fa6a97a599306e2566d4be426f
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2024-09-04 14:05:04 -0400

    Add automated benchmarks (#256)
    * Added initial benchmarks
    
    * Heat benchmarks
    
    * Moved file logging
    
    * Added a basic pipeline for benchmarks.
    
    * Full pipeline to markdown table
    
    * Better log organization and keeps tracks of solver steps
    
    * Added support for multiple sims
    
    * Delete Manifest.toml
    
    * Added projects to julia scripts
    
    * Explicitly match the period char
    
    * Overhauled config generation
    
    * Removed generated files
    
    * Overhauled configuration generation
    
    * Support for CUDA benchmarking
    
    * Deleted old benchmarking suite
    
    * Integrated DrWatson into benchmarks
    
    * Stronger config generation
    
    * Much stronger data collection
    
    * Added full README
    
    * Remove my email
    
    * Added compat entries
    
    * Moved more code into src
    
    * Shuffle main code around
    
    * More config tests
    
    * Clean up result processing
    
    * Testing of data aggregation
    
    * Add post processing scripts
    
    * Use Brusselator from the Canon
    
    * Add support for running tagged simulations
    
    * Add back brussel canon
    
    * Support full runs and single runs
    
    * Update config gen to support tagging
    
    * Added automated config generation
    
    * Added Cahn-Hilliard files
    
    * Cleaning up and more tests
    
    * Better parsing of config files
    
    * Better file accessing, more data collection
    
    * Support for main args for slurm
    
    * Updated readme for new workflow
    
    * Cleaning up
    
    ---------
    
    Co-authored-by: George Rauta <grauta@login12.ufhpc>
    Co-authored-by: George Rauta <grauta@login11.ufhpc>
    Co-authored-by: Luke Morris <70283489+lukem12345@users.noreply.github.com>

---
+++ benchmarks/scripts/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=scripts
+cd $DIR
+
+module load julia
+
+if [ $# == 3 ]
+then
+    SIMNAME=$1
+    ARCH=$2
+    TAG=$3
+    julia --threads=auto main.jl $SIMNAME $ARCH $TAG
+elif [ $# == 0 ]
+then
+    julia --threads=auto main.jl
+else
+  echo "Usage: ['physics' 'architecture' 'tag']"
+  exit 1
+fi
+
---
+++ benchmarks/src/physics/heat/heat.jl
@@ -0,0 +1,69 @@
+using DrWatson
+@quickactivate :benchmarks
+
+using ACSets
+using CUDA
+using CUDA.CUSPARSE
+using CombinatorialSpaces
+using ComponentArrays
+using Decapodes
+using DiagrammaticEquations
+using GeometryBasics: Point2, Point3
+using LinearAlgebra
+using MLStyle
+using OrdinaryDiffEq
+
+function pass_simulation_instance()
+  return SimulationInstance(setup_config, setup_benchmark, create_mesh, create_simulate, run_simulation)
+end
+
+struct HeatConfig
+  float_type
+  code_target
+  res
+end
+
+function setup_config(task_config_data::Dict{String, Any})
+  float_type = parse_float_type(task_config_data["float_type"])
+  code_target = parse_code_target(task_config_data["code_target"])
+  resolution = Float64(task_config_data["resolution"])
+  @info "Float type: $(float_type), Code target: $(code_target), Resolution: $(resolution)"
+
+  HeatConfig(float_type, code_target, resolution)
+end
+
+function setup_benchmark(config::HeatConfig)
+  Heat = @decapode begin
+      U::Form0
+      ∂ₜ(U) == 100 * Δ(U)
+  end
+
+  eval(gensim(Heat, code_target = config.code_target, stateeltype = config.float_type))
+end
+
+function create_mesh(config::HeatConfig)
+  s = triangulated_grid(100, 100, config.res, config.res, Point2{config.float_type})
+  sd = EmbeddedDeltaDualComplex2D{Bool, config.float_type, Point2{config.float_type}}(s)
+  subdivide_duals!(sd, Circumcenter())
+
+  U = map(sd[:point]) do (x,_)
+    return x
+  end
+  if config.code_target isa CUDATarget
+    U = CuArray(U)
+  end
+  u₀ = ComponentArray(U=U)
+
+  (sd, u₀, ())
+end
+
+function create_simulate(config::HeatConfig, sd, simulate)
+  simulate(sd, nothing, DiagonalHodge())
+end
+
+function run_simulation(config::HeatConfig, fm, u0, cnst_param)
+  tₑ = config.float_type(10)
+  prob = ODEProblem(fm, u0, (0, tₑ), cnst_param)
+
+  soln = solve(prob, Tsit5(), saveat=0.1)
+end
