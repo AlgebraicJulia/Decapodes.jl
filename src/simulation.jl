4ff298162c5db1b731e3d900e0a2da8bd8a05d10
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2024-10-31 17:39:22 -0400

    Fix spacing

--- src/simulation.jl
+++ src/simulation.jl
@@ -535,8 +535,8 @@ end
 A combined `infer_types` and `resolve_overloads` pipeline with default DEC rules.
 """
 function infer_overload_compiler!(d::SummationDecapode, dimension::Int)
-    infer_types!(d, dim = dimension)
-    resolve_overloads!(d, dim = dimension)
+  infer_types!(d, dim = dimension)
+  resolve_overloads!(d, dim = dimension)
 end
 
 """    link_contracted_operators!(d::SummationDecapode, code_target::AbstractGenerationTarget)
