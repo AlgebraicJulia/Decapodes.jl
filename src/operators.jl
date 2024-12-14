89a4686f9bba91b956b45df0722ab426f5273ba8
Matt Cuffaro <cuffaro.matt@gmail.com>
2024-12-14 07:46:07 -0500

    adding flat-sharp to operators dictionary

--- src/operators.jl
+++ src/operators.jl
@@ -74,6 +74,8 @@ function default_dec_matrix_generate(sd::HasDeltaSet, my_symbol::Symbol, hodge::
 
     :♭ => dec_♭(sd)
 
+    :♭♯ => ♭♯_mat(sd)
+
     # Averaging Operator
     :avg₀₁ => dec_avg₀₁(sd)
 
@@ -169,6 +171,7 @@ function default_dec_generate(sd::HasDeltaSet, my_symbol::Symbol, hodge::Discret
     :plus => (+)
     :(-) || :neg => x -> -1 .* x
     :ln => (x -> log.(x))
+    :mag => x -> norm.(x)
 
     _ => error("Unmatched operator $my_symbol")
   end
