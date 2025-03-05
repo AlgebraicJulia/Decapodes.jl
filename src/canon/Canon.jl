f7f0a62e1861474a3a1c5286f9334d6596b9e357
Matt Cuffaro <cuffaro.matt@gmail.com>
2025-03-05 12:06:51 -0500

    added spacing in @docapode

--- src/canon/Canon.jl
+++ src/canon/Canon.jl
@@ -3,29 +3,36 @@ module Canon
 using DiagrammaticEquations
 using DiagrammaticEquations.Deca
 
+using MLStyle
 using Markdown
 
 struct DecapodeModel
     value::Symbol
 end
 
+
 export @docapode
 
 function create_pode_expr(t) 
   modelname, source, desc, variable, pode = t
   variable = Symbol(variable)
-  Base.remove_linenums!(pode)
-
-  modeldef = sprint.(Base.show_unquoted,pode.args)
-  modeldef = join(map(modeldef) do line; "    $line
-                  " end, "\n")
+ 
+  modeldef = String[]; lnns = LineNumberNode[]
+  foreach(pode.args) do arg
+      if arg isa LineNumberNode
+          !isempty(lnns) && arg.line - last(lnns).line > 1 ? push!(modeldef, "") : nothing
+          push!(lnns, arg)
+      else
+          push!(modeldef, sprint(Base.show_unquoted, arg))
+      end
+  end
+  modeldef = join(map(modeldef) do line; "    $line" end, "\n")
 
   docstring = Markdown.parse("""
 
   **$modelname**
 
-  [Source]($source)
-
+  $(!isempty(source) ? """[Source]($source)""" : "")
   $desc
 
   **Model** 
