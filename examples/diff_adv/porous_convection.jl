444377c52c2739bf18f1cbfff9314073006cef82
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-03-10 15:57:28 -0400

    Updated simulation
    Using correct interpolation now along with some new constants.

--- examples/diff_adv/porous_convection.jl
+++ examples/diff_adv/porous_convection.jl
@@ -12,6 +12,7 @@ using ComponentArrays
 using OrdinaryDiffEq
 using SparseArrays
 using StaticArrays
+using StatsBase
 import CombinatorialSpaces.DiscreteExteriorCalculus: eval_constant_primal_form
 
 # Reference equations are at the link below, dual-time method is replaced with a Poisson equation solve
@@ -23,12 +24,10 @@ Porous_Convection = @decapode begin
   (g, qD)::Form1
 
   bound_T == adiabatic(T)
-
+  # Darcy flux
   ρ == g ∧ (αρ₀ * bound_T)
-
   P == Δ⁻¹(δ(ρ))
-
-  qD == -(k_ηf * (d(P) - ρ))
+  qD == -k_ηf * (d(P) - ρ)
 
   Adv == ⋆(interpolate(∧ᵈᵖ₁₁(⋆(d(bound_T)), qD)))
   Ṫ == -1/ϕ * Adv + λ_ρ₀Cp * Δ(bound_T)
@@ -47,33 +46,10 @@ s = triangulated_grid(lx, ly, dx, dy, Point3{Float64});
 sd = EmbeddedDeltaDualComplex2D{Bool, Float64, Point2{Float64}}(s);
 subdivide_duals!(sd, Circumcenter());
 
-# fig = Figure();
-# ax = CairoMakie.Axis(fig[1,1])
-# msh = mesh!(ax, s)
-# fig
-
 Δ0 = Δ(0,sd);
 fΔ0 = LinearAlgebra.factorize(Δ0);
 
-# This matrix is for interpolation of PrimalForm2 to DualForm2, meant for use with primal-primal Lie
-# TODO: To test, if there is a constant Form2, for example with just 1s, over primal triangles, primal vertex will have 1/(tri_area)
-# This assume triangle area and orientations are the same. So essentially, inv_hdq_0 * mat * ones() = 1 ./ sd[:area]
-mat = spzeros(nv(sd), ntriangles(sd))
-for tri_id in triangles(sd)
-  tri_area = sd[tri_id, :area]
-  tri_orient = sd[tri_id, :tri_orientation]
-  for dual_tri_id in tri_id:ntriangles(sd):nparts(sd, :DualTri)
-    dual_tri_area = sd[dual_tri_id, :dual_area]
-    dual_tri_orient = sd[dual_tri_id, :D_tri_orientation]
-
-    weight = tri_orient * (dual_tri_area / tri_area)
-
-    v = sd[sd[dual_tri_id, :D_∂e1], :D_∂v1]
-
-    mat[v, tri_id] += weight
-
-  end
-end
+mat = p2_d2_interpolation(sd)
 
 left_wall_idxs = findall(p -> p[1] < dx, s[:point]);
 right_wall_idxs = findall(p -> p[1] > lx - dx, s[:point]);
@@ -92,7 +68,7 @@ function generate(sd, my_symbol; hodge=GeometricHodge())
   op = @match my_symbol begin
     :Δ⁻¹ => x -> begin
       y = fΔ0 \ x
-      y .- minimum(y)
+      y .-= minimum(y)
     end
     :adiabatic => x -> begin
       x[left_wall_idxs] .= x[next_left_wall_idxs]
@@ -101,7 +77,7 @@ function generate(sd, my_symbol; hodge=GeometricHodge())
     end
     :tb_bc => apply_tb_bc
     :interpolate => x -> mat * x
-    _ => default_dec_matrix_generate(sd, my_symbol, hodge)
+    _ => error("No operator $my_symbol found.")
   end
   return op
 end
@@ -122,16 +98,16 @@ grav = SVector{3}([0.0, -accl_g, 0.0])
 g = eval_constant_primal_form(sd, grav)
 u₀ = ComponentArray(T=T, g=g)
 
-Ra = 1000
+Ra = 750
 k_ηf = 1.0
-αρ₀ = 1.0/accl_g
+αρ₀ = (1.0/accl_g)
 ϕ = 0.1
-λ_ρ₀Cp = 1/Ra*(3.92*αρ₀*k_ηf*ΔT*ly/ϕ)
+λ_ρ₀Cp = 1/Ra*(accl_g*αρ₀*k_ηf*ΔT*ly/ϕ)
 constants = (k_ηf = k_ηf, αρ₀ = αρ₀, ϕ = ϕ, λ_ρ₀Cp = λ_ρ₀Cp)
 
 # Smallest time step in original simulation was 0.00019, largest around 0.00100, around 0.00050 from original implementation
 # Only ran for 500 time steps, but adaptive time stepping means physical time simulated could vary
-tₑ = 2
+tₑ = 0.7
 
 prob = ODEProblem(f, u₀, (0, tₑ), constants)
 soln = solve(prob, Tsit5())
@@ -147,20 +123,32 @@ dp_wdg_11 = dec_wedge_product_dp(Tuple{1,1}, sd)
 inv_hdg_0 = dec_inv_hodge_star(0, sd)
 
 function calculate_pressure(T, constants)
-  boussinesq = wdg10(g, constants.αρ₀ * T)
-  P = fΔ0 \ (codif_1 * boussinesq) # Pressure
+  fΔ0 \ (codif_1 * wdg10(g, constants.αρ₀ * T))
 end
 
 function calculate_advection(T, P, constants)
-  boussinesq = wdg10(g, constants.αρ₀ * T)
-
-  darcy_flux = -(constants.k_ηf * (d0 * P - boussinesq))
-
-  Adv = apply_tb_bc(-1/constants.ϕ *  inv_hdg_0 * mat * dp_wdg_11(hdg_1 * d0 * T, darcy_flux))
+  darcy_flux = -constants.k_ηf * (d0 * P - wdg10(g, constants.αρ₀ * T))
+  apply_tb_bc(-1/constants.ϕ *  inv_hdg_0 * mat * dp_wdg_11(hdg_1 * d0 * T, darcy_flux))
 end
 
 function calculate_diffusion(T, constants)
-  Diff = apply_tb_bc(constants.λ_ρ₀Cp * Δ0 * T)
+  apply_tb_bc(constants.λ_ρ₀Cp * Δ0 * T)
+end
+
+function compute_colorranges(length)
+  values = hcat(map(range(0, soln.t[end], length=length)) do t
+    T = soln(t).T
+    P = calculate_pressure(T, constants)
+    Adv = calculate_advection(T, P, constants)
+    Diff = calculate_diffusion(T, constants)
+
+    [minimum(P), maximum(P), minimum(Adv), maximum(Adv), minimum(Diff), maximum(Diff)]
+  end...)
+
+  # Minimum, Maximum
+  percentile(values[1, :], 90), percentile(values[2, :], 90), # Pressure
+  percentile(values[3, :], 90), percentile(values[4, :], 90), # Advection
+  percentile(values[5, :], 75), percentile(values[6, :], 75) # Diffusion
 end
 
 function save_dynamics(save_file_name, video_length = 30)
@@ -171,22 +159,27 @@ function save_dynamics(save_file_name, video_length = 30)
   Adv = @lift(calculate_advection($T, $P, constants))
   Diff = @lift(calculate_diffusion($T, constants))
 
+  colorranges = compute_colorranges(video_length)
+  P_range = colorranges[1], colorranges[2]
+  Adv_range = colorranges[3], colorranges[4]
+  Diff_range = colorranges[5], colorranges[6]
+
   f = Figure()
 
   ax_T = CairoMakie.Axis(f[1,1], title = @lift("Temperature at Time $(round($time, digits=3))"))
-  msh_T = mesh!(ax_T, s, color=T, colormap=:jet)
+  msh_T = mesh!(ax_T, s; color=T, colormap=:jet, colorrange=(-ΔT/2, ΔT/2))
   Colorbar(f[1,2], msh_T)
 
   ax_P = CairoMakie.Axis(f[2,1], title = @lift("Pressure at Time $(round($time, digits=3))"))
-  msh_P = mesh!(ax_P, s, color=P, colormap=:jet)
+  msh_P = mesh!(ax_P, s; color=P, colormap=:jet, colorrange=P_range)
   Colorbar(f[2,2], msh_P)
 
   ax_Adv = CairoMakie.Axis(f[1,3], title = @lift("Advection at Time $(round($time, digits=3))"))
-  msh_Adv = mesh!(ax_Adv, s, color=Adv, colormap=:jet)
+  msh_Adv = mesh!(ax_Adv, s; color=Adv, colormap=:jet, colorrange=Adv_range)
   Colorbar(f[1,4], msh_Adv)
 
   ax_Diff = CairoMakie.Axis(f[2,3], title = @lift("Diffusion at Time $(round($time, digits=3))"))
-  msh_Diff = mesh!(ax_Diff, s, color=Diff, colormap=:jet)
+  msh_Diff = mesh!(ax_Diff, s; color=Diff, colormap=:jet, colorrange=Diff_range)
   Colorbar(f[2,4], msh_Diff)
 
   timestamps = range(0, soln.t[end], length=video_length)
@@ -195,4 +188,4 @@ function save_dynamics(save_file_name, video_length = 30)
   end
 end
 
-save_dynamics("Porous_Convection.mp4", 120)
+save_dynamics("Porous_Convection_New.mp4", 120)
