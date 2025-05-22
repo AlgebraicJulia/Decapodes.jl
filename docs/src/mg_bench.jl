53c19fd5fe1710b2ee26ce716f4cea82d9cf9c24
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-05-22 17:36:43 -0400

    Use Gauss-Seidel instead of CG as smoother
    Could also switch to SOR. Also included loop to reduce number of layers.

--- docs/src/mg_bench.jl
+++ docs/src/mg_bench.jl
@@ -2,14 +2,40 @@ using ACSets
 using CombinatorialSpaces
 using BenchmarkTools
 using LinearAlgebra
-using GeometryBasics: Point2, Point3
 using Krylov, KrylovPreconditioners
-const Point2D = Point2{Float64}
-const Point3D = Point3{Float64}
+using IterativeSolvers: gauss_seidel!, ssor!
+import CombinatorialSpaces.Multigrid: _multigrid_μ_cycle, multigrid_μ_cycles, multigrid_wcycles, car, cdr
+
+function _multigrid_μ_cycle(u, b, md::MultigridData, alg=gauss_seidel!, μ=1)
+  A,r,p,s = CombinatorialSpaces.Multigrid.car(md)
+  u = alg(u,A,b,maxiter=s)
+  length(md) == 1 && return u
+  r_f = b - A*u
+  r_c = r * r_f
+  z = _multigrid_μ_cycle(zeros(size(r_c)), r_c, cdr(md), alg, μ)
+  if μ > 1
+    z = _multigrid_μ_cycle(z, r_c, cdr(md), alg, μ-1)
+  end
+  u += p * z
+  u = alg(u,A,b,maxiter=s)
+end
+
+function multigrid_μ_cycles(u, b, md::MultigridData, cycles, alg=cg, μ=1)
+  cycles == 0 && return u
+  u = _multigrid_μ_cycle(u,b,md,alg,μ)
+  multigrid_μ_cycles(u,b,md,cycles-1,alg,μ)
+end
+
+multigrid_wcycles(u, b, md, cycles, alg=gauss_seidel!) = multigrid_μ_cycles(u, b, md, cycles, alg, 2)
+
+# Edit these constants for benchmarking!
+# TODO: Set levels to 7 and remove 3
+LEVELS = 5
+REMOVE_LAYERS = 3
 
 println("Generating Mesh")
 s = triangulated_grid(240, 240, 10, 10, Point2D);
-multi_mesh = PrimalGeometricMapSeries(s, binary_subdivision_map, 4);
+multi_mesh = PrimalGeometricMapSeries(s, binary_subdivision_map, LEVELS - 1);
 sd = finest_mesh(multi_mesh);
 
 println("Mesh elements:")
@@ -50,20 +76,25 @@ cdr2(md::MultigridData) =
     MultigridData(md.operators[1:end-1],md.restrictions[1:end-1],md.prolongations[1:end-1],md.steps[1:end-1]) :
     error("Not enough grids remaining in $md to take the cdr2.")
 
-# CombinatorialSpaces Multigrid
-println("Multigrid Solver")
-md = MGData(multi_mesh, sd -> ∇²(0, sd), 3, BinarySubdivision());
-warm_start = zeros(nv(sd));
-@btime mg_u = multigrid_wcycles(warm_start, f, md, 5);
-mg_u = multigrid_wcycles(warm_start, f, md, 5);
-@show rel_err(lap, mg_u, f)
-
-println("Geometric Multigrid Preconditioned CG")
 import LinearAlgebra: ldiv!
+
 function ldiv!(y, A::MultigridData, b)
-  y .= multigrid_wcycles(warm_start, b, A, 2);
+  y .= multigrid_wcycles(zeros(nv(sd)), b, A, 2);
 end
 
-@btime kr_mg_u, kr_mg_stats = cg(lap, f, M = md, ldiv = true);
-kr_mg_u, kr_mg_stats = cg(lap, f, M = md, ldiv = true);
-@show rel_err(lap, kr_mg_u, f)
+md = MGData(multi_mesh, sd -> ∇²(0, sd), 3, BinarySubdivision());
+for i in 1:REMOVE_LAYERS+1
+  # CombinatorialSpaces Multigrid
+  println("Multigrid Solver, with $(LEVELS - i + 1) layers")
+  @btime mg_u = multigrid_wcycles(zeros(nv(sd)), f, md, 5);
+  mg_u = multigrid_wcycles(zeros(nv(sd)), f, md, 5);
+  @show rel_err(lap, mg_u, f)
+
+  println("Geometric Multigrid Preconditioned CG, with $(LEVELS - i + 1) layers")
+
+  @btime kr_mg_u, kr_mg_stats = cg(lap, f, M = md, ldiv = true);
+  kr_mg_u, kr_mg_stats = cg(lap, f, M = md, ldiv = true);
+  @show rel_err(lap, kr_mg_u, f)
+
+  md = cdr2(md)
+end
