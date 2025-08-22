40c8398d271bc011b9ca4b69d9e2c08e1ec78250
GeorgeR227 <grauta@ufl.edu>
2025-08-22 13:14:52 -0400

    Have dual sim running, bcs likely causing mass gain

--- examples/diff_adv/compressible_dual.jl
+++ examples/diff_adv/compressible_dual.jl
@@ -35,10 +35,8 @@ inv_hdg_2 = dec_inv_hodge_star(2, sd, DiagonalHodge())
 wdg_10 = dec_wedge_product(Tuple{1,0}, sd, Val{:CUDA}, CuArray, Float64)
 wdg_11 = dec_wedge_product(Tuple{1,1}, sd, Val{:CUDA}, CuArray, Float64)
 
-# TODO: Running d0_p0 directly is causing OutOfMemory errors
-# interp = d0_p0_interpolation(sd; hodge=DiagonalHodge())
-interp = SparseMatrixCSC{Float64}(inv_hdg_0) * p2_d2_interpolation(sd) * SparseMatrixCSC{Float64}(inv_hdg_2)
-♭♯ = ♭♯_mat(sd)
+interp = d0_p0_interpolation(sd; hodge=DiagonalHodge())
+flatsharp = ♭♯_mat(sd)
 
 struct TaylorVortexParams
   G::Real
@@ -171,12 +169,13 @@ function plot_zeroform(s, f)
   ax = CairoMakie.Axis(fig[1, 1])
   msh = CairoMakie.mesh!(ax, s, color=isdual ? interp * f : f, colormap=:jet)
   Colorbar(fig[1, 2], msh)
-  display(fig)
+  # display(fig)
+  save("view.png", fig)
 end
 
 # TODO: This interpolates the dual velocity field
 function plot_velocity_diff(sd, dual_u, u)
-  dual_u♯ = pp♯ * ♭♯ * dual_u
+  dual_u♯ = pp♯ * flatsharp * dual_u
   primal_u♯ = pp♯ * u
 
   u♯ = primal_u♯ .- dual_u♯
@@ -208,19 +207,19 @@ plot_zeroform(s, ω)
 
 plot_zeroform(s, ψ)
 
-function p0_d0_interpolation(sd::HasDeltaSet2D)
-  m = spzeros(ntriangles(sd), nv(sd))
-  for tri in triangles(sd)
-    tri_area = sd[tri, :area]
-    for i in 0:5
-      dual_tri = tri + i * ntriangles(sd)
-      v, _ = dual_triangle_vertices(sd, dual_tri)
-      dual_tri_area = sd[dual_tri, :dual_area]
-      m[tri, v] += dual_tri_area / tri_area
-    end
-  end
-  m
-end
+# function p0_d0_interpolation(sd::HasDeltaSet2D)
+#   m = spzeros(ntriangles(sd), nv(sd))
+#   for tri in triangles(sd)
+#     tri_area = sd[tri, :area]
+#     for i in 0:5
+#       dual_tri = tri + i * ntriangles(sd)
+#       v, _ = dual_triangle_vertices(sd, dual_tri)
+#       dual_tri_area = sd[dual_tri, :dual_area]
+#       m[tri, v] += dual_tri_area / tri_area
+#     end
+#   end
+#   m
+# end
 
 boundary_edges = findall(x -> x != 0, dual_d0 * ones(ntriangles(sd)))
 
@@ -250,29 +249,33 @@ U₀ = wdg_10dd(CuArray{Float64}(u), ρ₀)
 
 m₀ = CUDA.sum(ρ₀)
 
-form_one_interp_1 = CuSparseMatrixCSC{Float64}(inv_hdg_1)
-form_one_interp_2 = CuSparseMatrixCSC{Float64}(hdg_1)
-form_zero_interp = CuSparseMatrixCSC{Float64}(inv_hdg_0 * dual_d1)
-form_two_interp = CuSparseMatrixCSC{Float64}(dual_d0 * hdg_2)
+invhdg1 = CuSparseMatrixCSC{Float64}(inv_hdg_1)
+hdg1 = CuSparseMatrixCSC{Float64}(hdg_1)
+invhdg0_dd1 = CuSparseMatrixCSC{Float64}(inv_hdg_0 * dual_d1)
 
 special_dual_d0 = deepcopy(dual_d0)
 special_dual_d0[boundary_edges, :] .= 0
 cu_dual_d0 = CuSparseMatrixCSC{Float64}(special_dual_d0)
 
-codif_1 = CuSparseMatrixCSC{Float64}(abs.(hdg_2)) * CuSparseMatrixCSC{Float64}(d1) * CuSparseMatrixCSC{Float64}(inv_hdg_1)
+dd0_hdg2 = CuSparseMatrixCSC{Float64}(special_dual_d0 * hdg_2)
+
+codif_1 = CuSparseMatrixCSC{Float64}(hdg_2) * CuSparseMatrixCSC{Float64}(d1) * invhdg1
 
 lap_first_term = CuSparseMatrixCSC{Float64}(hdg_1 * d0 * inv_hdg_0 * dual_d1)
 lap_second_term = CuSparseMatrixCSC{Float64}(special_dual_d0 * hdg_2 * d1 * inv_hdg_1)
 
 lap_term = lap_first_term + lap_second_term
 
+cu_flatsharp = CuSparseMatrixCSC{Float64}(flatsharp)
+
 function momentum_continuity(U, ρ)
   u = wdg_10dd(U, 1 ./ ρ)
+  v = cu_flatsharp * u
 
   return -wdg_10dd(U, codif_1 * u) - # U ∧ δu
-         form_two_interp * wdg_11(u, form_one_interp_1 * U) - # L(u, U)
-         form_one_interp_2 * wdg_10(u, form_zero_interp * U) -
-         0.5 * wdg_10dd(form_two_interp * wdg_11(u, form_one_interp_1 * u), ρ) + # 1/2 * ρ * d||u||^2
+         dd0_hdg2 * wdg_11(v, invhdg1 * U) - # L(u, U)
+         hdg1 * wdg_10(v, invhdg0_dd1 * U) +
+         0.5 * wdg_10dd(dd0_hdg2 * wdg_11(v, invhdg1 * u), ρ) - # 1/2 * ρ * d||u||^2
          cu_dual_d0 * (κ * ρ) + # dP, P = κρ
          μ * lap_term * u # μΔu
 
@@ -291,8 +294,8 @@ function run_compressible_ns(U₀, ρ₀, tₑ, Δt; saveat=500)
 
   steps = ceil(Int64, tₑ / Δt)
 
-  Us = [deepcopy(U₀)]
-  rhos = [deepcopy(ρ₀)]
+  Us = [Array(U₀)]
+  rhos = [Array(ρ₀)]
 
   for step in 1:steps
     U_half .= U .+ 0.5 * Δt * momentum_continuity(U, ρ)
@@ -317,8 +320,8 @@ function run_compressible_ns(U₀, ρ₀, tₑ, Δt; saveat=500)
     end
 
     if step % saveat == 0
-      push!(Us, deepcopy(U))
-      push!(rhos, deepcopy(ρ))
+      push!(Us, Array(U))
+      push!(rhos, Array(ρ))
       println("Loading simulation results: $((step / steps) * 100)%")
       println("Relative mass is : $((CUDA.sum(ρ) / m₀) * 100)%")
       println("-----")
@@ -328,35 +331,29 @@ function run_compressible_ns(U₀, ρ₀, tₑ, Δt; saveat=500)
   return Us, rhos
 end
 
-μ = 0 # \Re
+Re = 1000
+μ = 1 / Re
 
-tₑ = 1
+tₑ = 5
 Δt = 1e-4 # dx / 360
-Us, rhos = run_compressible_ns(U₀, ρ₀, tₑ, Δt; saveat=500);
+Us, rhos = run_compressible_ns(U₀, ρ₀, tₑ, Δt; saveat=250);
+
+cpu_wdg_10_dd_mat = wedge_dd_01_mat(sd)
+cpu_wdg_10dd(α, f) = α .* (cpu_wdg_10_dd_mat * f)
 
 timestep = length(Us)
-u_end = Array(wdg_10dd(Us[timestep], 1 ./ rhos[timestep]))
+u_end = cpu_wdg_10dd(Us[timestep], 1 ./ rhos[timestep])
 ω_end = inv_hdg_0 * dual_d1 * u_end
 
-fig = Figure();
-ax = CairoMakie.Axis(fig[1, 1])
-msh = CairoMakie.mesh!(ax, s, color=ω_end, colormap=:jet)
-Colorbar(fig[1, 2], msh)
-display(fig)
-
-# plot_primal_vorticity(sd, u_end)
-
-fig = Figure();
-ax = CairoMakie.Axis(fig[1, 1])
-msh = CairoMakie.mesh!(ax, s, color=interp * Array(rhos[timestep]), colormap=:jet)
-Colorbar(fig[1, 2], msh)
-display(fig)
+plot_zeroform(s, ω_end)
+plot_zeroform(s, interp * rhos[timestep])
+plot_zeroform(s, hdg_2 * d1 * inv_hdg_1 * u_end)
 
 function save_dynamics(save_file_name)
   time = Observable(1)
 
-  w = @lift(inv_hdg_0 * dual_d1 * Array(wdg_10dd(Us[$time], 1 ./ rhos[$time])))
-  ρ = @lift(interp * Array(rhos[$time]))
+  w = @lift(inv_hdg_0 * dual_d1 * cpu_wdg_10dd(Us[$time], 1 ./ rhos[$time]))
+  ρ = @lift(interp * rhos[$time])
 
   f = Figure(size=(1000, 1000))
 
@@ -365,11 +362,11 @@ function save_dynamics(save_file_name)
   Colorbar(f[1, 2], msh_w)
 
   ax_ρ = CairoMakie.Axis(f[2, 1], title="Density with μ=$(μ)")
-  msh_ρ = mesh!(ax_ρ, s; color=ρ, colormap=:jet, colorrange=(-1e-6, 1e-6) .+ 1)
+  msh_ρ = mesh!(ax_ρ, s; color=ρ, colormap=:jet, colorrange=(-1e-5, 1e-5) .+ 1)
   Colorbar(f[2, 2], msh_ρ)
 
-  colsize!(fig.layout, 1, Aspect(1, 1.0))
-  resize_to_layout!(fig)
+  colsize!(f.layout, 1, Aspect(1, 1.0))
+  resize_to_layout!(f)
 
   timestamps = 1:length(Us)
   CairoMakie.record(f, save_file_name, timestamps; framerate=15) do t
